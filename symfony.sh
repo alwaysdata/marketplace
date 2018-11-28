7d103ac983daeeab092e43f036041aa3f7a17f03
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2018-11-28 15:00:22 +0100

    Add Symfony

---
+++ symfony.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# site:
+#     type: php
+#     path: '{INSTALL_PATH_RELATIVE}/public/'
+#     php_version: '7.2'
+
+set -e
+
+# https://symfony.com/doc/current/setup.html
+composer create-project symfony/website-skeleton default
+cd default
+composer require symfony/web-server-bundle
+
+cd ..
+rm -rf .composer
+
+shopt -s dotglob nullglob
+mv default/* .
+rmdir default
