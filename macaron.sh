df03c3170c821a334520a570d5f09303cd83fd01
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2018-11-28 15:05:14 +0100

    Add Macaron

---
+++ macaron.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# site:
+#     type: user_program
+#     working_directory: '{INSTALL_PATH}'
+#     command: '~{INSTALL_PATH_RELATIVE}/hello'
+#     path_trim: true
+
+set -e
+
+export GOPATH=$INSTALL_PATH
+
+# https://go-macaron.com/docs/intro/getting_started
+
+go get gopkg.in/macaron.v1
+
+cat << EOF > hello.go
+package main
+
+import (
+    "os"
+    "gopkg.in/macaron.v1" 
+)
+ 
+func main() { 
+    m := macaron.Classic()    
+    m.Get("/", func() string {
+        return "Hello world!"
+    })  
+
+    m.Run(os.Getenv("IP"), os.Getenv("PORT"))
+}
+EOF
+
+go build hello.go
+
+rm -rf pkg src hello.go
