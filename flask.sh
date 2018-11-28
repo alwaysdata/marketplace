893b46c3becdff997199e4c7df042a9572bb1078
m4dz <code@m4dz.net>
2018-11-28 14:24:12 +0100

    Add Flask and Sinatra scripts

---
+++ flask.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# site:
+#     python_version: '3.7'
+#     type: 'wsgi'
+#     working_directory: '{INSTALL_PATH_RELATIVE}'
+#     virtualenv_directory: '{INSTALL_PATH_RELATIVE}/env'
+#     path: '{INSTALL_PATH_RELATIVE}:app'
+#     path_trim: true
+
+set -e
+
+# http://flask.pocoo.org/docs/1.0/quickstart/
+
+python -m venv env
+env/bin/pip install Flask
+
+cat << EOF > __init__.py
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('hello.html')
+
+if __name__ == "__main__":
+  app.run(debug=True)
+
+EOF
+
+mkdir templates
+cat << EOF > templates/hello.html
+<!doctype html>
+<title>Hello from Flask</title>
+<h1>Hello, World!</h1>
+
+EOF
---
+++ sinatra.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# site:
+#     ruby_version: '2.5'
+#     type: 'ruby_rack'
+#     working_directory: '{INSTALL_PATH_RELATIVE}'
+#     path: '{INSTALL_PATH_RELATIVE}/config.ru'
+#     path_trim: true
+
+set -e
+
+# http://sinatrarb.com/documentation.html
+
+cat << EOF > Gemfile
+source "http://rubygems.org"
+
+gem 'sinatra', require: 'sinatra/base'
+gem 'sinatra-reloader', require: 'sinatra/reloader'
+
+EOF
+bundle install --path vendor/bundle
+
+cat << EOF > server.rb
+# Requires the Gemfile
+require 'bundler' ; Bundler.require
+
+# Sinatra server
+module Server
+  class App < Sinatra::Base
+    register Sinatra::Reloader
+
+    get '/' do
+      erb :index
+    end
+  end
+end
+
+EOF
+
+cat << EOF > config.ru
+require './server.rb'
+
+run Server::App
+
+EOF
+
+mkdir views
+cat << EOF > views/index.erb
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Hello World!</title>
+</head>
+
+<body>
+	<h1>Hello World!</h1>
+	<p>Today is <%= Time.now.strftime('%A') %></p>
+</body>
+
+</html>
+
+EOF
