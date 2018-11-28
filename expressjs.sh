cc7f7500a8115edfcbc0cd5fe61227d3f15f9fe1
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2018-11-28 15:03:28 +0100

    Add Express.js

---
+++ expressjs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# site:
+#     type: nodejs
+#     node_version: '11'
+#     working_directory: '{INSTALL_PATH}'
+#     command: 'node ~{INSTALL_PATH_RELATIVE}/app.js'
+#     path_trim: true
+
+set -e
+
+# http://expressjs.com/en/starter/installing.html
+
+npm init -y
+npm install express --save
+
+# http://expressjs.com/en/starter/hello-world.html
+
+cat << EOF > app.js
+const express = require('express')
+
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(process.env.PORT, '0.0.0.0', function () {
+  console.log('Example app started!')
+})
+EOF
