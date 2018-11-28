a10c0919b992e2fb3a41f19f8fb767faac9c6005
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2018-11-28 14:59:32 +0100

    Add Laravel

---
+++ laravel.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# site:
+#     type: php
+#     path: '{INSTALL_PATH_RELATIVE}/public/'
+#     php_version: '7.2'
+
+set -e
+
+# https://laravel.com/docs/
+
+composer create-project --prefer-dist laravel/laravel default
+# composer require/laravel new fails because of tty
+
+sed -i "s|APP_URL=http://localhost|APP_URL=http://$INSTALL_URL|" default/.env
+
+rm -rf .composer .subversion vendor composer.json composer.lock
+
+shopt -s dotglob nullglob
+mv default/* .
+rmdir default
