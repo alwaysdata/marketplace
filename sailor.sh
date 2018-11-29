7d51972506376fcd50f538f15cbda1e855c387e4
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2018-11-29 16:38:45 +0100

    Add Sailor

---
+++ sailor.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# site:
+#     type: user_program
+#     working_directory: '{INSTALL_PATH}'
+#     command: '~{INSTALL_PATH_RELATIVE}/start-server.sh'
+#     path_trim: true
+
+set -e
+
+LUAROCKS="luarocks --tree=$INSTALL_PATH/.luarocks"
+$LUAROCKS install sailor
+.luarocks/bin/sailor create hello
+
+sed -i 's|host = "\*"|host = "0.0.0.0"|'  hello/start-server.lua
+sed -i 's|port = 8080|port = os.getenv("PORT")|'  hello/start-server.lua
+
+cat << EOF > hello/start-server.sh
+#!/bin/sh
+$($LUAROCKS path --bin)
+exec lua5.1 start-server.lua
+EOF
+
+chmod +x hello/start-server.sh
+
+shopt -s dotglob nullglob
+mv hello/* .
+rmdir hello
