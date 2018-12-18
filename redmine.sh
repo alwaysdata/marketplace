dbdd3dc8ab1fea01686799aa8c0882701b8ff7c3
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2018-12-18 11:10:06 +0100

    add Redmine (version 4.0.0)

---
+++ redmine.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# site:
+#     type: ruby_rack
+#     ruby_version: '2.5'
+#     path: '{INSTALL_PATH_RELATIVE}/config.ru'
+#     environment: 'RAILS_ENV=production'
+#     bundler: true
+#     path_trim: true
+# database:
+#     type: postgresql
+# form:
+#     language:
+#         type: choices
+#         label: Language
+#         initial: en
+#         choices:
+#             de: German
+#             en: English
+#             es: Spanish
+#             fr: French
+#             it: Italian
+
+set -e
+
+wget -O- http://www.redmine.org/releases/redmine-4.0.0.tar.gz | tar -xz --strip-components=1
+
+# Database configuration
+cat << EOF > config/database.yml
+production:
+  adapter: postgresql
+  database: $DATABASE_NAME
+  host: $DATABASE_HOST
+  username: $DATABASE_USERNAME
+  password: "$DATABASE_PASSWORD"
+EOF
+
+# Email configuration
+cat << EOF > config/configuration.yml
+production:
+  email_delivery:
+    delivery_method: :smtp
+    smtp_settings:
+      address: $SMTP_HOST
+      port: 25
+      domain: $INSTALL_URL_HOSTNAME
+EOF
+
+bundle install --path vendor/bundle --without development test
+bundle exec rake generate_secret_token
+
+RAILS_ENV=production bundle exec rake db:migrate
+RAILS_ENV=production REDMINE_LANG=$FORM_LANGUAGE bundle exec rake redmine:load_default_data
