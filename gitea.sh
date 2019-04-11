90bc42679c4daa599e426a11f6843c5d002ac227
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2019-04-11 12:09:52 +0200

    Upgrade Gitea to 1.7.5

--- gitea.sh
+++ gitea.sh
@@ -12,7 +12,7 @@ set -e
 
 # https://docs.gitea.io/en-us/install-from-binary/
 
-wget -qO gitea https://dl.gitea.io/gitea/1.7.4/gitea-1.7.4-linux-amd64
+wget -qO gitea https://dl.gitea.io/gitea/1.7.5/gitea-1.7.5-linux-amd64
 chmod +x gitea
 
 mkdir -p custom/conf data indexers public log
