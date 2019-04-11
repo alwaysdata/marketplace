f6e40fbb2c0bf0677e9d1bf572519f66c7a40258
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2019-04-11 14:53:52 +0200

    Update and Upgrage Omeka S

--- omeka-s.sh
+++ omeka-s.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # site:
 #     type: php
@@ -7,6 +7,16 @@
 # database:
 #     type: mysql
 # form:
+#     language:
+#         type: choices
+#         label: Language
+#         initial: en_US
+#         choices:
+#             de_DE: German
+#             en_US: English
+#             es: Spanish
+#             fr: French
+#             it: Italian
 #     title:
 #         label: Title
 #         max_length: 255
@@ -23,7 +33,7 @@
 
 set -e
 
-wget -O- https://github.com/omeka/omeka-s/archive/v1.3.0.tar.gz | tar -xz --strip-components=1
+wget -O- https://github.com/omeka/omeka-s/archive/v1.4.0.tar.gz | tar -xz --strip-components=1
 
 # https://omeka.org/s/docs/user-manual/install/
 
@@ -38,4 +48,4 @@ EOF
 
 mv .htaccess.dist  .htaccess
 
-curl -X POST -F user[email]="$FORM_EMAIL" -F user[email-confirm]="$FORM_EMAIL" -F user[password]="$FORM_ADMIN_PASSWORD" -F user[password-confirm]="$FORM_ADMIN_PASSWORD" -F user[name]="$FORM_ADMIN_USERNAME" -F settings[installation_title]="$FORM_TITLE" -F settings[time_zone]=Europe/Paris -F submit=Submit http://$INSTALL_URL/install
+curl -X POST -F user[email]="$FORM_EMAIL" -F user[email-confirm]="$FORM_EMAIL" -F user[name]="$FORM_ADMIN_USERNAME" -F user[password-confirm][password]="$FORM_ADMIN_PASSWORD" -F user[password-confirm][password-confirm]="$FORM_ADMIN_PASSWORD" -F settings[installation_title]="$FORM_TITLE" -F settings[time_zone]=Europe/Paris -F settings[locale]="$FORM_LANGUAGE" -F submit=Submit http://$INSTALL_URL/install
