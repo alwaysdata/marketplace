ac62357029e5870300ef7c20e2bce46867633c4e
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2019-06-12 15:11:49 +0200

    Modify script and upgrade Phoenix to 1.4.7

--- phoenix.sh
+++ phoenix.sh
@@ -16,12 +16,31 @@ export MIX_HOME=$INSTALL_PATH/.mix
 
 echo 'Y' | mix local.hex 
 mix local.rebar 
-echo 'Y' | mix archive.install hex phx_new 1.4.3
+echo 'Y' | mix archive.install hex phx_new 1.4.7
 echo 'Y' | mix phx.new hello --no-ecto 
 cd hello
 
-sed -i 's|:inet6|:inet|' config/prod.exs
+cat << EOF > config/prod.secret.exs
+# In this file, we load production configuration and
+# secrets from environment variables. You can also
+# hardcode secrets, although such is generally not
+# recommended and you have to remember to add this
+# file to your .gitignore.
+use Mix.Config
+
+secret_key_base = '$(mix phx.gen.secret| sha256sum -b | sed 's/ .*//')'
+#  System.get_env("SECRET_KEY_BASE") ||
+#    raise """
+#    environment variable SECRET_KEY_BASE is missing.
+#    You can generate one by calling: mix phx.gen.secret
+#    """
+
+config :hello, HelloWeb.Endpoint,
+  http: [:inet, port: String.to_integer(System.get_env("PORT") || "4000")],
+  secret_key_base: secret_key_base
+EOF
 
 mix deps.get
+
 MIX_ENV=prod mix compile
 MIX_ENV=prod mix phx.digest
