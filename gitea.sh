f89a58a96d4f7aa2f6cb393a4d2e46b537c0a357
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2019-09-11 15:19:56 +0200

    Upgrade Gitea to 1.9.3

--- gitea.sh
+++ gitea.sh
@@ -12,7 +12,7 @@ set -e
 
 # https://docs.gitea.io/en-us/install-from-binary/
 
-wget -qO gitea https://dl.gitea.io/gitea/1.9.1/gitea-1.9.1-linux-amd64
+wget -qO gitea https://dl.gitea.io/gitea/1.9.3/gitea-1.9.3-linux-amd64
 chmod +x gitea
 
 mkdir -p custom/conf data indexers public log
