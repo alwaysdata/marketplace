70f6611a38b916b89464ee6d3692236ab5ca1e92
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2019-11-18 14:40:13 +0100

    upgrade Gitea to 1.10.0

--- gitea.sh
+++ gitea.sh
@@ -12,7 +12,7 @@ set -e
 
 # https://docs.gitea.io/en-us/install-from-binary/
 
-wget -qO gitea https://dl.gitea.io/gitea/1.9.4/gitea-1.9.4-linux-amd64
+wget -qO gitea https://dl.gitea.io/gitea/1.10.0/gitea-1.10.0-linux-amd64
 chmod +x gitea
 
 mkdir -p custom/conf data indexers public log
