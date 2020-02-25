96b307d7705934e193a9464f47ef6827c950965b
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2020-02-25 14:16:08 +0100

    update Symfony script

--- symfony.sh
+++ symfony.sh
@@ -3,16 +3,13 @@
 # site:
 #     type: php
 #     path: '{INSTALL_PATH_RELATIVE}/public/'
-#     php_version: '7.3'
+#     php_version: '7.4'
 
 set -e
 
 # https://symfony.com/doc/current/setup.html
 composer create-project symfony/website-skeleton default
-cd default
-composer require symfony/web-server-bundle
 
-cd ..
 rm -rf .composer
 
 shopt -s dotglob nullglob
