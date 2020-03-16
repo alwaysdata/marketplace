1f4541e141ae8423a787fbdc1336a3dd0b1df359
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2020-03-16 12:18:45 +0100

    upgrade et migration buster

--- etherpad.sh
+++ etherpad.sh
@@ -2,7 +2,7 @@
 
 # site:
 #     type: nodejs
-#     nodejs_version: '9'
+#     nodejs_version: '12'
 #     working_directory: '{INSTALL_PATH}'
 #     command: 'bin/run.sh'
 # database:
@@ -10,13 +10,13 @@
 
 set -e
 
-wget -O- https://github.com/ether/etherpad-lite/archive/1.7.5.tar.gz | tar -xz --strip-components=1
+wget -O- https://github.com/ether/etherpad-lite/archive/1.8.0.tar.gz | tar -xz --strip-components=1
 
 cp settings.json.template settings.json
 
 cat << EOF > credentials.json
 {
-"ip": "$IP",
+"ip": "$(test $(head -c1 /etc/debian_version) == 8 && echo '0.0.0.0' || echo '')",
 "port" : $PORT,
 
 "dbType" : "mysql",
