759fdb387a3f033144176a9d00ed15dde43a2482
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2020-03-31 14:12:08 +0200

    simplify Sailor

--- sailor.sh
+++ sailor.sh
@@ -12,91 +12,16 @@ LUAROCKS="luarocks --tree=$INSTALL_PATH/.luarocks"
 $LUAROCKS install sailor
 .luarocks/bin/sailor create hello
 
-# during the buster migration
-#sed -i 's|host = "\*"|host = "0.0.0.0"|'  hello/start-server.lua
-#sed -i 's|port = 8080|port = os.getenv("PORT")|'  hello/start-server.lua
-
-cat << EOF > hello/start-server.lua
-local xavante = require "xavante" 
-local filehandler = require "xavante.filehandler"
-local cgiluahandler = require "xavante.cgiluahandler"
-local redirect = require "xavante.redirecthandler"
-local conf = (require "conf.conf").sailor
-
--- Define here where Xavante HTTP documents scripts are located
-local webDir = "."
-
-local uri_map
-
-if conf.friendly_urls then
-    uri_map = { -- URI remapping
-        match = "^[^%./]*/?([^%.]*)$",
-        with = redirect,
-        params = {
-            "/",
-            function(req,res,cap)
-                local vars = {}
-
-                for var in string.gmatch(cap[1], '([^/]+)') do
-                    table.insert(vars,var)
-                end
-
-                if #vars > 0 then
-                    local mod = (#vars % 2) - 1
-                    local get = ""
-
-                    if #vars > 1 - mod then
-                        for i = 2 - mod, #vars, 2 do
-                            get = get.."&"..vars[i].."="..vars[i+1]
-                        end
-                    end
-
-                    if mod == -1 then
-                        get = vars[2]..get
-                    end
-
-                    req.cmd_url = "/index.lua?"..conf.route_parameter.."="..vars[1].."/"..get
-                else
-                    req.cmd_url = "/index.lua"
-                end
-               
-                return "reparse"
-            end
-        }
-    }
+## Buster hotfix
+if [ $(head -c1 /etc/debian_version) -eq 8 ]
+then
+    export IP='0.0.0.0'
 else
-    uri_map = {
-        match = "^[^%./]*/$",
-        with = redirect,
-        params = {"index.lua"}
-      }
-end
+    export IP='::'
+fi
 
-local simplerules = {
-    
-    uri_map,
-
-    { -- cgiluahandler example
-      match = {"%.lp$", "%.lp/.*$", "%.lua$", "%.lua/.*$" },
-      with = cgiluahandler.makeHandler (webDir)
-    },
-    
-    { -- filehandler example
-      match = ".",
-      with = filehandler,
-      params = {baseDir = webDir}
-    },
-} 
-
-xavante.HTTP{
-    server = {host = "$(test $(head -c1 /etc/debian_version) == 8 && echo '0.0.0.0' || echo '::')", port = os.getenv("PORT")},
-    
-    defaultHost = {
-      rules = simplerules
-    }
-}
-xavante.start()
-EOF
+sed -i "s|host = \"\*\"|host = \"$IP\"|"  hello/start-server.lua
+sed -i 's|port = 8080|port = os.getenv("PORT")|'  hello/start-server.lua
 
 cat << EOF > hello/start-server.sh
 #!/bin/sh
