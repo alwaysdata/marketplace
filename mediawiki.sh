4b7a88a912abf0f378b70426f874e3f6faa30621
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2020-04-06 11:09:49 +0200

    Upgrade Mediawiki to 1.34.1 + script update

--- mediawiki.sh
+++ mediawiki.sh
@@ -27,23 +27,19 @@
 #     admin_password:
 #         type: password
 #         label: Administrator password
+#         min_lenght: 10
 #         max_length: 255
 
 set -e
 
-wget -O- https://github.com/wikimedia/mediawiki/archive/1.34.0.tar.gz | tar -xz --strip-components=1
+wget -O- https://releases.wikimedia.org/mediawiki/1.34/mediawiki-1.34.1.tar.gz | tar -xz --strip-components=1
 
 composer install
-php maintenance/install.php --dbname="$DATABASE_NAME" --installdbpass="$DATABASE_PASSWORD" --dbserver="$DATABASE_HOST" --installdbuser="$DATABASE_USERNAME" --dbuser="$DATABASE_USERNAME" --dbpass="$DATABASE_PASSWORD" --dbprefix=wiki_ --lang="$FORM_LANGUAGE" --pass="$FORM_ADMIN_PASSWORD" --server="http://$INSTALL_URL_HOSTNAME" --scriptpath="$INSTALL_URL_PATH" "$FORM_TITLE" "$FORM_ADMIN_USERNAME"
+php maintenance/install.php --dbname="$DATABASE_NAME" --installdbpass="$DATABASE_PASSWORD" --dbserver="$DATABASE_HOST" --installdbuser="$DATABASE_USERNAME" --dbuser="$DATABASE_USERNAME" --dbpass="$DATABASE_PASSWORD" --dbprefix=wiki --lang="$FORM_LANGUAGE" --pass="$FORM_ADMIN_PASSWORD" --server="http://$INSTALL_URL_HOSTNAME" --scriptpath="$INSTALL_URL_PATH" --skins=Vector "$FORM_TITLE" "$FORM_ADMIN_USERNAME"
 
 if [ "$INSTALL_URL_PATH" = "/" ]
 then
     sed -i 's|\$wgScriptPath = "/";|\$wgScriptPath = "";|' LocalSettings.php
 fi
-
-# Install the default skin (no skin is installed by default):
-# https://www.mediawiki.org/wiki/Skin:Vector
-git clone https://gerrit.wikimedia.org/r/mediawiki/skins/Vector skins/Vector
-sed -i "/monobook/a wfLoadSkin( 'Vector' );" LocalSettings.php
 
 rm -rf .composer
