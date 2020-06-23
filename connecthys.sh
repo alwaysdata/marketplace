5aea6281858711cbf740275b797e61f99db9a541
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2020-06-23 09:58:46 +0200

    Upgrade Connecthys to 0.7.5

--- connecthys.sh
+++ connecthys.sh
@@ -17,7 +17,7 @@ source env/bin/activate
 
 pip install pycrypto flask six
 
-wget -O- https://github.com/Noethys/Connecthys/archive/0.7.4.tar.gz | tar -xz --strip-components=1
+wget -O- https://github.com/Noethys/Connecthys/archive/0.7.5.tar.gz | tar -xz --strip-components=1
 
 shopt -s dotglob nullglob
 mv connecthys/* .
