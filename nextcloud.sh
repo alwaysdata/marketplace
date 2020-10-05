faf801616ec3f636f822b3d14670691338271e91
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2020-10-05 10:43:01 +0200

    Upgrade Nextcloud to 20.0.0

--- nextcloud.sh
+++ nextcloud.sh
@@ -18,11 +18,11 @@
 
 set -e
 
-# https://docs.nextcloud.com/server/18/admin_manual/configuration_server/occ_command.html?highlight=occ#command-line-installation-label
+# https://docs.nextcloud.com/server/20/admin_manual/configuration_server/occ_command.html?highlight=occ#command-line-installation-label
 
-wget -O- https://github.com/nextcloud/server/archive/v19.0.3.tar.gz | tar -xz --strip-components=1
+wget -O- https://github.com/nextcloud/server/archive/v20.0.0.tar.gz | tar -xz --strip-components=1
 # Although not specified in the documentation, the 3rdparty directory must be downloaded separately, otherwise the occ command will complain.
-wget -O- https://github.com/nextcloud/3rdparty/archive/v19.0.3.tar.gz | tar -C 3rdparty -xz --strip-components=1
+wget -O- https://github.com/nextcloud/3rdparty/archive/v20.0.0.tar.gz | tar -C 3rdparty -xz --strip-components=1
 
 composer install
 
