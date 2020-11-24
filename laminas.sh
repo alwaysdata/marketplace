213ab16d9d898d5f254a755cb1804dbe046bda5c
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2020-11-24 13:57:58 +0100

    Laminas: composer cache directory & addresses

--- laminas.sh
+++ laminas.sh
@@ -3,19 +3,18 @@
 # site:
 #     type: php
 #     path: '{INSTALL_PATH_RELATIVE}/public/'
-#     addresses: '{INSTALL_URL}/web'
 #     php_version: '7.4'
 
 set -e
 
 # https://docs.laminas.dev/tutorials/getting-started/skeleton-application/
 
-echo 'Y' | composer create-project -s dev laminas/laminas-mvc-skeleton default
+echo 'Y' | COMPOSER_CACHE_DIR=/dev/null composer create-project -s dev laminas/laminas-mvc-skeleton default
 
 shopt -s dotglob nullglob
 mv default/* .
 rmdir default
 
-composer development-disable
+COMPOSER_CACHE_DIR=/dev/null composer development-disable
 
 rm -rf .composer .subversion
