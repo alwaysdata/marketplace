94e9f3354b4bcdd1c9460d3f527724b4e5b3600a
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2021-02-10 09:54:14 +0100

    WP_SITEURL only temporarily following issue on ticket - change of address

--- wordpress.sh
+++ wordpress.sh
@@ -37,10 +37,12 @@ set -e
 # We use http://wp-cli.org
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
+# Temporarily add WP_SITEURL in wp-config.php because of https://github.com/wp-cli/core-command/issues/51
 php -d sys_temp_dir=/tmp wp-cli.phar core download --locale="$FORM_LANGUAGE" --path="$INSTALL_PATH"
 php wp-cli.phar config create --dbname="$DATABASE_NAME" --dbuser="$DATABASE_USERNAME" --dbpass="$DATABASE_PASSWORD" --dbhost="$DATABASE_HOST" --path="$INSTALL_PATH" --extra-php <<PHP
 define( 'WP_SITEURL', 'http://$INSTALL_URL' );
 PHP
 php wp-cli.phar core install --url="$INSTALL_URL" --title="$FORM_TITLE" --admin_user="$FORM_ADMIN_USERNAME" --admin_password="$FORM_ADMIN_PASSWORD" --admin_email="$FORM_EMAIL" --path="$INSTALL_PATH"
-    
+
+sed -i '/WP_SITEURL/d' wp-config.php
 rm -rf .wp-cli wp-cli.phar
