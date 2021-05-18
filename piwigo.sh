b6334081fc64791da7a08f0bcad5043bf7716ce8
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2021-05-18 11:00:50 +0200

    Upgrade Piwigo to 11.5.0

--- piwigo.sh
+++ piwigo.sh
@@ -31,6 +31,6 @@
 
 set -e
 
-wget -O- https://github.com/Piwigo/Piwigo/archive/refs/tags/11.4.0.tar.gz| tar -xz --strip-components=1
+wget -O- https://github.com/Piwigo/Piwigo/archive/refs/tags/11.5.0.tar.gz| tar -xz --strip-components=1
 
 curl -X POST -F language="$FORM_LANGUAGE" -F dbhost="$DATABASE_HOST" -F dbuser="$DATABASE_USERNAME" -F dbpasswd="$DATABASE_PASSWORD" -F dbname="$DATABASE_NAME" -F admin_name="$FORM_USERNAME" -F admin_pass1="$FORM_PASSWORD" -F admin_pass2="$FORM_PASSWORD" -F admin_mail="$FORM_EMAIL" -F newsletter_subscribe=none -F send_credentials_by_mail=checked -F install=Start\ installation http://$INSTALL_URL/install.php
