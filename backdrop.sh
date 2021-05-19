bf40f5beec8e1286bb259b3a2a106e46bd8ebc06
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2021-05-19 11:47:45 +0200

    Backdrop: removal of nullglob and space

--- backdrop.sh
+++ backdrop.sh
@@ -11,10 +11,10 @@ set -e
 
 COMPOSER_CACHE_DIR=/dev/null composer2 create-project backdrop/backdrop default
 
- sed -i "s|user:pass@localhost/database_name|$DATABASE_USERNAME:$DATABASE_PASSWORD@$DATABASE_HOST/$DATABASE_NAME|" default/settings.php
+sed -i "s|user:pass@localhost/database_name|$DATABASE_USERNAME:$DATABASE_PASSWORD@$DATABASE_HOST/$DATABASE_NAME|" default/settings.php
 
 rm -rf .config .local .subversion
-shopt -s dotglob nullglob
+shopt -s dotglob
 mv default/* .
 rmdir default
 
