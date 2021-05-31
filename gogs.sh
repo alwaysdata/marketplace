9605a3ac4faf6c7f73d812de48cbf2d2959d19a1
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2021-05-31 15:21:42 +0200

    cat -> curl

--- gogs.sh
+++ gogs.sh
@@ -17,54 +17,10 @@ mkdir -p custom/conf
 cat << EOF > custom/conf/app.ini
 
 RUN_MODE = prod
-RUN_USER  = $USER
-BRAND_NAME = Gogs
-
 [server]
 HTTP_PORT = $PORT
 DOMAIN = 0.0.0.0
 EXTERNAL_URL = http://$INSTALL_URL/
-DISABLE_SSH = false
-SSH_PORT = 22
-START_SSH_SERVER = false
-OFFLINE_MODE = false
-
-[repository]
-ROOT = $INSTALL_PATH/gogs-repositories
-
-[database]
-TYPE = postgres
-HOST = $DATABASE_HOST:5432
-NAME = $DATABASE_NAME
-USER = $DATABASE_USERNAME
-PASSWORD = $DATABASE_PASSWORD
-SSL_MODE = disable
-PATH = $INSTALL_PATH/data/gogs.db
-
-[mailer]
-ENABLED = false
-
-[service]
-REGISTER_EMAIL_CONFIRM = false
-ENABLE_NOTIFY_MAIL = false
-DISABLE_REGISTRATION = false
-ENABLE_CAPTCHA = true
-REQUIRE_SIGNIN_VIEW = false
-
-[picture]
-DISABLE_GRAVATAR = false
-ENABLE_FEDERATED_AVATAR = false
-
-[session]
-PROVIDER = file
-
-[log]
-MODE      = file
-LEVEL     = Info
-ROOT_PATH = $INSTALL_PATH/log
-
-[security]
-INSTALL_LOCK = true
-SECRET_KEY   = '$(date | sha256sum -b | sed 's/ .*//')'
 EOF
 
+curl -X POST -F db_type=PostgreSQL -F db_host=$DATABASE_HOST:5432 -F db_user=$DATABASE_USERNAME -F db_passwd=$DATABASE_PASSWORD -F db_name=$DATABASE_NAME -F app_name=Gogs -F repo_root_path=$INSTALL_PATH/gogs-repositories -F run_user=$USER -F domain=0.0.0.0 -F http_port=$PORT -F app_url=http://$INSTALL_URL/ -F log_root_path=$INSTALL_PATH/log -F submit="Installer Gogs" http://$INSTALL_URL/install
