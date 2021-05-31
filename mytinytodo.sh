fe1630a488cba85ab6b81ce228e1a964bbc8dbcf
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2021-05-31 15:21:27 +0200

    cat -> sed

--- mytinytodo.sh
+++ mytinytodo.sh
@@ -22,38 +22,13 @@ set -e
 
 wget -O- https://github.com/maxpozdeev/mytinytodo/releases/download/v1.6.4/mytinytodo-v1.6.4.zip | bsdtar --strip-components=1 -xf -
 
-mv db/config.php db/config.template.php
-
-cat << EOF > db/config.php
-<?php
-\$config = array();
-\$config['db'] = 'mysql';
-\$config['mysql.host'] = '$DATABASE_HOST';
-\$config['mysql.db'] = '$DATABASE_NAME';
-\$config['mysql.user'] = '$DATABASE_USERNAME';
-\$config['mysql.password'] = '$DATABASE_PASSWORD';
-\$config['prefix'] = 'mtt_';
-\$config['url'] = '';
-\$config['mtt_url'] = '';
-\$config['title'] = '';
-\$config['lang'] = '$FORM_LANGUAGE';
-\$config['password'] = '';
-\$config['smartsyntax'] = 1;
-\$config['timezone'] = 'Europe/Paris';
-\$config['autotag'] = 1;
-\$config['duedateformat'] = 1;
-\$config['firstdayofweek'] = 1;
-\$config['session'] = 'files';
-\$config['clock'] = 24;
-\$config['dateformat'] = 'j M Y';
-\$config['dateformat2'] = 'n/j/y';
-\$config['dateformatshort'] = 'j M';
-\$config['template'] = 'default';
-\$config['showdate'] = 0;
-\$config['detectmobile'] = 1;
-?>
-
-EOF
+sed -i "s|\['db'\] = '';|\['db'\] = 'mysql';|" db/config.php
+sed -i "s|localhost|$DATABASE_HOST|" db/config.php
+sed -i "s|\['mysql.db'\] = \"mytinytodo\";|\['mysql.db'\] = '$DATABASE_NAME';|" db/config.php
+sed -i "s|\['mysql.user'\] = \"user\"|\['mysql.user'\] = '$DATABASE_USERNAME';|" db/config.php
+sed -i "s|\['mysql.password'\] = \"\";|\['mysql.password'\] = '$DATABASE_PASSWORD';|" db/config.php
+sed -i "s|\['lang'\] = \"en\"|\['lang'\] = \"$FORM_LANGUAGE\"|" db/config.php
+sed -i "s|\['timezone'\] = 'UTC'|\['timezone'\] = 'Europe/Paris'|" db/config.php
 
 curl -X POST -F installdb=mysql -F mysql_host='$DATABASE_HOST' -F mysql-db='$DATABASE_NAME' -F mysql_user='$DATABASE_USERNAME' -F mysql_password='$DATABASE_PASSWORD' -F submit=Next https://$INSTALL_URL/setup.php
 curl -X POST -F install=Install https://$INSTALL_URL/setup.php
