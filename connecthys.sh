225c9340594e36104d5b47c18390ba71c7e9563c
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2021-05-31 12:29:47 +0200

    Upgrade Connecthys to 0.9.3

--- connecthys.sh
+++ connecthys.sh
@@ -17,6 +17,6 @@ source env/bin/activate
 
 pip install pycrypto flask six
 
-wget -O- https://github.com/Noethys/Connecthys/archive/0.9.1.tar.gz | tar -xz --strip-components=2
+wget -O- https://github.com/Noethys/Connecthys/archive/0.9.3.tar.gz | tar -xz --strip-components=2
 
 chmod 755 connecthys.wsgi run.py
