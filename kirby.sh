a89d7f26dc44b03e026bcc7115a3bb66ae1e5d53
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2021-06-10 14:03:14 +0200

    Add Kirby CMS

---
+++ kirby.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# site:
+#     type: php
+#     path: '{INSTALL_PATH_RELATIVE}'
+#     php_version: '8'
+
+set -e
+
+COMPOSER_CACHE_DIR=/dev/null composer2 create-project getkirby/starterkit default
+
+rm -rf .config .local .subversion
+shopt -s dotglob
+mv default/* .
+rmdir default
