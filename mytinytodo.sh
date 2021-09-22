196296e6ee3f9c0fb8f61fc627e68701ad62a11a
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2021-09-22 11:19:44 +0200

    Upgrade myTinyTodo to 1.6.6

--- mytinytodo.sh
+++ mytinytodo.sh
@@ -20,7 +20,7 @@
 
 set -e
 
-wget -O- https://github.com/maxpozdeev/mytinytodo/releases/download/v1.6.5/mytinytodo-v1.6.5.zip | bsdtar --strip-components=1 -xf -
+wget -O- https://github.com/maxpozdeev/mytinytodo/releases/download/v1.6.6/mytinytodo-v1.6.6.zip | bsdtar --strip-components=1 -xf -
 
 sed -i "s|\['db'\] = '';|\['db'\] = 'mysql';|" db/config.php
 sed -i "s|localhost|$DATABASE_HOST|" db/config.php
