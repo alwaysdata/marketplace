802d7e61001a5eb7f52cd8f92e9092648fa3afb5
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2021-09-28 10:23:45 +0200

    Add Leantime

---
+++ leantime.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# site:
+#     type: php
+#     path: '{INSTALL_PATH_RELATIVE}'
+#     php_version: '7.4'
+# database:
+#     type: mysql
+
+set -e
+
+wget -O- https://github.com/Leantime/leantime/releases/download/v2.1.8/Leantime-v2.1.8.tar.gz | tar -xz --strip-components=1
+
+cp config/configuration.sample.php config/configuration.php
+
+sed -i "s|dbHost=\"localhost\"|dbHost=\"$DATABASE_HOST\"|" config/configuration.php
+sed -i "s|dbUser=\"\"|dbUser=\"$DATABASE_USERNAME\"|" config/configuration.php
+sed -i "s|dbPassword=\"\"|dbPassword=\"$DATABASE_PASSWORD\"|" config/configuration.php
+sed -i "s|dbDatabase=\"\"|dbDatabase=\"$DATABASE_NAME\"|" config/configuration.php
+
+if [ "$INSTALL_URL_PATH" != "/" ]
+then
+	sed -i "s|#RewriteBase /leantime/|RewriteBase $INSTALL_URL_PATH|" public/.htaccess
+	sed -i "s|appUrl = \"\"|appUrl = \"$INSTALL_URL_PATH\"|" config/configuration.php
+fi
+
+# after GUI interface to specify login username
