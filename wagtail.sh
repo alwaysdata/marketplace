65cbebcf2ef4605a8c1f26bf90fd0f8d7aa93d87
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2021-11-15 09:57:38 +0100

    Wagtail: first admin user

--- wagtail.sh
+++ wagtail.sh
@@ -15,13 +15,32 @@
 #             fr: Nom du projet
 #         regex: ^[a-zA-Z0-9_]+$
 #         max_length: 255
+#     email:
+#         type: email
+#         label:
+#             en: Email
+#             fr: Email
+#     admin_username:
+#         label:
+#             en: Administrator username
+#             fr: Nom d'utilisateur de l'administrateur
+#         max_length: 255
+#     admin_password:
+#         type: password
+#         label:
+#             en: Administrator password
+#             fr: Mot de passe de l'administrateur
+#         max_length: 255
 
 set -e
 
 python -m venv env
 source env/bin/activate
 
-pip install wagtail
+python -m pip install wagtail
 ./env/bin/wagtail start $FORM_PROJECT .
 
 python manage.py migrate
+
+# First admin user
+DJANGO_SUPERUSER_PASSWORD="$FORM_ADMIN_PASSWORD" python manage.py createsuperuser --username $FORM_ADMIN_USERNAME --email $FORM_EMAIL --noinput
