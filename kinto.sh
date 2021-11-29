680abc3a1e19d5edef30b974b99be6ecab5b2220
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2021-11-29 11:35:52 +0100

    take-back Kinto & wallabag

---
+++ kinto.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# site:
+#     type: wsgi
+#     path: '{INSTALL_PATH_RELATIVE}/app.wsgi'
+#     python_version: '3.9'
+#     virtualenv_directory: '{INSTALL_PATH_RELATIVE}/env'
+#     working_directory: '{INSTALL_PATH_RELATIVE}'
+#     ssl_force: true
+# database:
+#     type: postgresql
+# requirements:
+#     disk: 85
+
+set -e
+
+# Python environment
+python -m venv env
+source env/bin/activate
+python -m pip install kinto[postgresql]
+
+# WSGI
+wget https://raw.githubusercontent.com/Kinto/kinto/master/app.wsgi
+
+# Application configuration
+kinto init --ini config/kinto.ini --backend postgresql --cache-backend postgresql
+sed -i "s|postgresql://postgres:postgres@localhost/postgres|postgresql://$DATABASE_USERNAME:$DATABASE_PASSWORD@$DATABASE_HOST/$DATABASE_NAME|" config/kinto.ini
+kinto migrate --ini config/kinto.ini
---
+++ wallabag.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# site:
+#     type: php
+#     path: '{INSTALL_PATH_RELATIVE}/web/'
+#     php_version: '7.4'
+#     php_ini: extension=intl.so
+# database:
+#     type: mysql
+# requirements:
+#     disk: 200
+# form:
+#     language:
+#         type: choices
+#         label:
+#             en: Language
+#             fr: Langue
+#         choices:
+#             de: Deutsch 
+#             en: English
+#             es: Español
+#             fr: Français
+#             it: Italiano
+#     admin_username:
+#         label:
+#             en: Administrator username
+#             fr: Nom d'utilisateur de l'administrateur
+#         regex: ^[ a-zA-Z0-9.@_-]+$
+#         max_length: 255
+#     admin_password:
+#         label:
+#             en: Administrator password
+#             fr: Mot de passe de l'administrateur
+#         type: password
+#         min_lenght: 5
+#         max_length: 255
+#     admin_email:
+#         type: email
+#         label:
+#             en: Email
+#             fr: Email
+
+set -e
+
+wget -qO- https://static.wallabag.org/releases/wallabag-release-2.4.2.tar.gz | tar -xz --strip-components=1
+
+sed -i "s|database_host: 127.0.0.1|database_host: $DATABASE_HOST|" app/config/parameters.yml
+sed -i "s|database_name: wallabag|database_name: $DATABASE_NAME|" app/config/parameters.yml
+sed -i "s|database_user: root|database_user: $DATABASE_USERNAME|" app/config/parameters.yml
+sed -i "s|database_password: null|database_password: '$DATABASE_PASSWORD'|" app/config/parameters.yml
+sed -i "s|your-wallabag-url-instance.com|$INSTALL_URL|" app/config/parameters.yml
+sed -i "s|locale: en|locale: $FORM_LANGUAGE|" app/config/parameters.yml
+
+bin/console cache:clear --env=prod
+bin/console wallabag:install --env=prod -n
+bin/console fos:user:create --env=prod "$FORM_ADMIN_USERNAME" "$FORM_ADMIN_EMAIL" "$FORM_ADMIN_PASSWORD"
