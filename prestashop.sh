da40fe6bfe1eea2ed94e62c77a35d17a86721c2a
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2021-12-17 12:36:32 +0100

    PrestaShop: Remove the language selection
    Whatever the language chosen, the PrestaShop administration interface always used the default language (English)

--- prestashop.sh
+++ prestashop.sh
@@ -10,17 +10,6 @@
 # requirements:
 #     disk: 440
 # form:
-#     language:
-#         type: choices
-#         label:
-#             en: Language
-#             fr: Langue
-#         choices:
-#             de: Deutsch
-#             en: English
-#             es: Español
-#             fr: Français
-#             it: Italiano
 #     shop_name:
 #         label:
 #             en: Shop name
@@ -59,6 +48,6 @@ wget -O- https://github.com/PrestaShop/PrestaShop/releases/download/1.7.8.2/pres
 unzip -o prestashop.zip
 rm prestashop.zip
 
-php install/index_cli.php --domain="$INSTALL_URL_HOSTNAME" --base_uri="$INSTALL_URL_PATH" --language="$FORM_LANGUAGES" --db_name="$DATABASE_NAME" --db_user="$DATABASE_USERNAME" --db_password="$DATABASE_PASSWORD" --db_server="$DATABASE_HOST" --name="$FORM_SHOP_NAME" --firstname="$FORM_ADMIN_FIRSTNAME" --lastname="$FORM_ADMIN_LASTNAME" --password="$FORM_ADMIN_PASSWORD" --email="$FORM_EMAIL" --newsletter=0
+php install/index_cli.php --domain="$INSTALL_URL_HOSTNAME" --base_uri="$INSTALL_URL_PATH" --db_name="$DATABASE_NAME" --db_user="$DATABASE_USERNAME" --db_password="$DATABASE_PASSWORD" --db_server="$DATABASE_HOST" --name="$FORM_SHOP_NAME" --firstname="$FORM_ADMIN_FIRSTNAME" --lastname="$FORM_ADMIN_LASTNAME" --password="$FORM_ADMIN_PASSWORD" --email="$FORM_EMAIL" --newsletter=0
 
 rm -rf install
