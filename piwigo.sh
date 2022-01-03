ed525426df03d6ab7e68436cd17b4e06934db8d3
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-01-03 13:50:23 +0100

    Piwigo: upgrade + removal of the language selection
    Whatever the language chosen, Piwigo always used the English language

--- piwigo.sh
+++ piwigo.sh
@@ -9,17 +9,6 @@
 # requirements:
 #     disk: 35
 # form:
-#     language:
-#         type: choices
-#         label:
-#             en: Language
-#             fr: Langue
-#         choices:
-#             de_DE: Deutsch
-#             en_US: English
-#             es_ES: Español
-#             fr_FR: Français
-#             it_IT: Italiano
 #     email:
 #         type: email
 #         label:
@@ -40,6 +29,6 @@
 
 set -e
 
-wget -O- https://github.com/Piwigo/Piwigo/archive/refs/tags/12.1.0.tar.gz| tar -xz --strip-components=1
+wget -O- https://github.com/Piwigo/Piwigo/archive/refs/tags/12.2.0.tar.gz| tar -xz --strip-components=1
 
-curl -X POST -F language="$FORM_LANGUAGE" -F dbhost="$DATABASE_HOST" -F dbuser="$DATABASE_USERNAME" -F dbpasswd="$DATABASE_PASSWORD" -F dbname="$DATABASE_NAME" -F admin_name="$FORM_USERNAME" -F admin_pass1="$FORM_PASSWORD" -F admin_pass2="$FORM_PASSWORD" -F admin_mail="$FORM_EMAIL" -F newsletter_subscribe=none -F send_credentials_by_mail=checked -F install=Start\ installation http://$INSTALL_URL/install.php
+curl -X POST -F language="en_US" -F dbhost="$DATABASE_HOST" -F dbuser="$DATABASE_USERNAME" -F dbpasswd="$DATABASE_PASSWORD" -F dbname="$DATABASE_NAME" -F admin_name="$FORM_USERNAME" -F admin_pass1="$FORM_PASSWORD" -F admin_pass2="$FORM_PASSWORD" -F admin_mail="$FORM_EMAIL" -F newsletter_subscribe=none -F send_credentials_by_mail=checked -F install=Start\ installation http://$INSTALL_URL/install.php
