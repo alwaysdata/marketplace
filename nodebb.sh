59a704d4df40a26f6432d45fa8a18bb825069969
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-02-10 12:03:28 +0100

    Upgrade NodeBB to 1.19.2

--- nodebb.sh
+++ nodebb.sh
@@ -31,7 +31,7 @@
 #         max_length: 255
 set -e
 
-wget -O- https://github.com/NodeBB/NodeBB/archive/refs/tags/v1.19.1.tar.gz|tar -xz --strip-components=1
+wget -O- https://github.com/NodeBB/NodeBB/archive/refs/tags/v1.19.2.tar.gz|tar -xz --strip-components=1
 
 cat << EOF > config.json
 {
@@ -54,6 +54,8 @@ export admin__username="$FORM_ADMIN_USERNAME"
 export admin__email="$FORM_EMAIL"
 export admin__password="$FORM_ADMIN_PASSWORD"
 export admin__password__confirm="$FORM_ADMIN_PASSWORD"
+
+npm install lodash --save
 ./nodebb setup
 
 sed -i "s|'0.0.0.0'|'::'|" install/web.js
