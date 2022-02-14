d76890030a74f2b6b010b66efafc81efed72c51b
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-02-14 14:44:41 +0100

    Strapi: Adding --ignore-existing to remove local bins from PATH and fix API error on admin dashboard
    https://forum.strapi.io/t/getting-warning-an-error-occurred-while-requesting-the-api-when-opening-strapi/13873

--- strapi.sh
+++ strapi.sh
@@ -2,7 +2,7 @@
 
 # site:
 #     type: nodejs
-#     nodejs_version: '16'
+#     nodejs_version: '14'
 #     working_directory: '{INSTALL_PATH}/strapi'
 #     command: 'npm run start'
 #     path_trim: true
@@ -13,6 +13,16 @@
 
 set -e
 
-echo "y"|npx create-strapi-app strapi --quickstart --no-run
+echo "y"|npx --ignore-existing create-strapi-app strapi --quickstart --no-run
 
-sed -i  "/port: env.int('PORT', 1337),/a \ \ url: 'https://$INSTALL_URL'," strapi/config/server.js
+cd strapi
+
+sed -i  "/port: env.int('PORT', 1337),/a \ \ url: 'https://$INSTALL_URL'," config/server.js
+
+cat << EOF |sed -i "/module.exports = ({ env }) => ({/r /dev/stdin" config/admin.js
+  apiToken: {
+    salt: env('API_TOKEN_SALT', '$(date +%s | sha256sum | base64 | head -c 32 ; echo)'),
+  },
+EOF
+
+npm run build
