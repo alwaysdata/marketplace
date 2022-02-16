59ceb3a24526ff0f1198adbffa9878c5be4d2d24
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-02-16 13:51:20 +0100

    Upgrade Nextcloud to 23.0.1

--- nextcloud.sh
+++ nextcloud.sh
@@ -25,9 +25,9 @@ set -e
 
 # https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/occ_command.html?highlight=occ#command-line-installation-label
 
-wget -O- https://github.com/nextcloud/server/archive/v23.0.0.tar.gz | tar -xz --strip-components=1
+wget -O- https://github.com/nextcloud/server/archive/v23.0.1.tar.gz | tar -xz --strip-components=1
 # Although not specified in the documentation, the 3rdparty directory must be downloaded separately, otherwise the occ command will complain.
-wget -O- https://github.com/nextcloud/3rdparty/archive/v23.0.0.tar.gz | tar -C 3rdparty -xz --strip-components=1
+wget -O- https://github.com/nextcloud/3rdparty/archive/v23.0.1.tar.gz | tar -C 3rdparty -xz --strip-components=1
 
 COMPOSER_CACHE_DIR=/dev/null composer2 install
 
