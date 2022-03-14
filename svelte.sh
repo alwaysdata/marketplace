58a4d7ba8f0949c462325b5b80b90e8d647b02cb
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-03-14 11:55:27 +0100

    Svelte: remove subdirectory, update HOME environment variables and disk requirements

--- svelte.sh
+++ svelte.sh
@@ -3,14 +3,21 @@
 # site:
 #     type: nodejs
 #     nodejs_version: '16'
-#     working_directory: '{INSTALL_PATH}/svelte-app'
+#     working_directory: '{INSTALL_PATH}'
 #     command: 'npm run dev'
-#     environment: HOME='{INSTALL_PATH}'
+#     environment: HOME={INSTALL_PATH}
 # requirements:
-#     disk: 40
+#     disk: 30
 
 set -e
 
 echo "y"|npx degit sveltejs/template svelte-app
 cd svelte-app
 npm install
+
+cd ..
+
+rm -rf  .npm
+shopt -s dotglob
+mv svelte-app/* .
+rmdir svelte-app
