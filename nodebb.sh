0c2d4449efb6b5c569e721ca5c0a1eabc26d14ab
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-05-09 11:04:40 +0200

    Upgrade NodeBB to 2.0.0

--- nodebb.sh
+++ nodebb.sh
@@ -31,7 +31,7 @@
 #         max_length: 255
 set -e
 
-wget -O- https://github.com/NodeBB/NodeBB/archive/refs/tags/v1.19.7.tar.gz|tar -xz --strip-components=1
+wget -O- https://github.com/NodeBB/NodeBB/archive/refs/tags/v2.0.0.tar.gz|tar -xz --strip-components=1
 
 cat << EOF > config.json
 {
