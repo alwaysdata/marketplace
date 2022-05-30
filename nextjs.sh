d550d05f4d8e4a4f31d4351d0fa687b0399e8e48
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-05-30 11:55:11 +0200

    Next.js: Handle the URL with subpath

--- nextjs.sh
+++ nextjs.sh
@@ -13,6 +13,16 @@ set -e
 
 echo "y"|npx create-next-app nextjs-project --use-npm --example "https://github.com/vercel/next-learn/tree/master/basics/learn-starter"
 
+
+if [ "$INSTALL_URL_PATH" != "/" ]
+then
+cat << EOF > next.config.js
+module.exports = {
+  basePath: '$INSTALL_URL_PATH',
+}
+EOF
+fi
+
 rm -rf  .npm
 shopt -s dotglob
 mv nextjs-project/* .
