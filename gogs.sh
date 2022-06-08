19f671c26c35aaefa583a52642a01616d5052530
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-06-08 10:06:37 +0200

    Upgrade Gogs to 0.12.9

--- gogs.sh
+++ gogs.sh
@@ -12,7 +12,7 @@
 
 set -e
 
-wget -O- https://github.com/gogs/gogs/releases/download/v0.12.8/gogs_0.12.8_linux_amd64.tar.gz|tar -xz --strip-components=1
+wget -O- https://github.com/gogs/gogs/releases/download/v0.12.9/gogs_0.12.9_linux_amd64.tar.gz|tar -xz --strip-components=1
 
 mkdir -p custom/conf
 
