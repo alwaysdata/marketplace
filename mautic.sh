125a39cd02f46df97a50720f6022e472582cebc0
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-06-28 10:44:09 +0200

    Upgrade Mautic to 4.4.0

--- mautic.sh
+++ mautic.sh
@@ -3,7 +3,7 @@
 # site:
 #     type: php
 #     path: '{INSTALL_PATH_RELATIVE}'
-#     php_version: '7.4'
+#     php_version: '8.0'
 #     php_ini: |
 #         memory_limit = 4096M
 #         extension = imap.so
@@ -35,7 +35,7 @@
 
 set -e
 
-wget -O- https://github.com/mautic/mautic/releases/download/4.3.1/4.3.1.zip | bsdtar --strip-components=0 -xf -
+wget -O- https://github.com/mautic/mautic/releases/download/4.4.0/4.4.0.zip | bsdtar --strip-components=0 -xf -
 
 php bin/console mautic:install --db_driver="pdo_mysql" --db_host="$DATABASE_HOST" --db_port="3306" --db_name="$DATABASE_NAME" --db_user="$DATABASE_USERNAME" --db_password="$DATABASE_PASSWORD" --admin_username="$FORM_ADMIN_USERNAME" --admin_email="$FORM_EMAIL" --admin_password="$FORM_ADMIN_PASSWORD" --mailer_from_email="$USER@$RESELLER_DOMAIN" --mailer_from_name="Mautic" https://$INSTALL_URL
 
