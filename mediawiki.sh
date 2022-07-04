11321bb7dd2f72b479b5a1d51b02b18490af89ad
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-07-04 11:41:44 +0200

    Upgrade MediaWiki to 1.38.2

--- mediawiki.sh
+++ mediawiki.sh
@@ -43,7 +43,7 @@ set -e
 
 # https://www.mediawiki.org/wiki/Compatibility
 
-wget -O- https://releases.wikimedia.org/mediawiki/1.38/mediawiki-1.38.1.tar.gz | tar -xz --strip-components=1
+wget -O- https://releases.wikimedia.org/mediawiki/1.38/mediawiki-1.38.2.tar.gz | tar -xz --strip-components=1
 
 COMPOSER_CACHE_DIR=/dev/null composer install
 php maintenance/install.php --dbname="$DATABASE_NAME" --installdbpass="$DATABASE_PASSWORD" --dbserver="$DATABASE_HOST" --installdbuser="$DATABASE_USERNAME" --dbuser="$DATABASE_USERNAME" --dbpass="$DATABASE_PASSWORD" --dbprefix=wiki --lang="$FORM_LANGUAGE" --pass="$FORM_ADMIN_PASSWORD" --server="http://$INSTALL_URL_HOSTNAME" --scriptpath="$INSTALL_URL_PATH" --skins=Vector "$FORM_TITLE" "$FORM_ADMIN_USERNAME"
