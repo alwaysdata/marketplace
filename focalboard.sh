b0d552becbf116f5bbfe5666d7cecf3f929eecfc
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-08-17 09:31:41 +0200

    Upgrade Focalboard to 7.2.0 + changing tmp directory

--- focalboard.sh
+++ focalboard.sh
@@ -10,11 +10,9 @@
 
 set -e
 
-wget -O- https://github.com/mattermost/focalboard/releases/download/v7.1.0/focalboard-server-linux-amd64.tar.gz|tar -xz --strip-components=1
+wget -O- https://github.com/mattermost/focalboard/releases/download/v7.2.0/focalboard-server-linux-amd64.tar.gz|tar -xz --strip-components=1
 
 sed -i "s|localhost:8000|$INSTALL_URL|" config.json
 sed -i "/$INSTALL_URL\",/a\ \ \ \ \ \ \ \ \"ip\": \"::\"," config.json
 sed -i "s|\"port\": 8000,|\"port\": $PORT,|" config.json
-sed -i "s|/var/tmp/focalboard_local.socket|var/tmp/focalboard_local.socket|" config.json
-
-mkdir -p var/tmp
+sed -i "s|/var/tmp/focalboard_local.socket|/home/$USER/admin/tmp/focalboard_local.socket|" config.json
