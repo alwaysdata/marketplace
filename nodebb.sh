6aef971980367ebd9c7d44a19c128bc6ac53bb5c
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-08-18 11:56:32 +0200

    Upgrade NodeBB to 2.4.3

--- nodebb.sh
+++ nodebb.sh
@@ -31,7 +31,7 @@
 #         max_length: 255
 set -e
 
-wget -O- https://github.com/NodeBB/NodeBB/archive/refs/tags/v2.4.1.tar.gz|tar -xz --strip-components=1
+wget -O- https://github.com/NodeBB/NodeBB/archive/refs/tags/v2.4.3.tar.gz|tar -xz --strip-components=1
 
 cat << EOF > config.json
 {
