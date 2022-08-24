e732a3b127b23c7ac5e1fa2aa7a21b8671c21ea6
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-08-24 16:10:08 +0200

    Strapi: upgrade Node.js version to 16

--- strapi.sh
+++ strapi.sh
@@ -2,7 +2,7 @@
 
 # site:
 #     type: nodejs
-#     nodejs_version: '14'
+#     nodejs_version: '16'
 #     working_directory: '{INSTALL_PATH}'
 #     command: 'npm run start'
 #     path_trim: true
