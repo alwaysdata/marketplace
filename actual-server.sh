3bcda2e216324fde627c50e7602724318c5efa13
m4dz <code@m4dz.net>
2022-09-06 13:53:50 +0200

    (add) Actual Budget server v1.0.3

---
+++ actual-server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# site:
+#     type: nodejs
+#     nodejs_version: '16'
+#     working_directory: '{INSTALL_PATH}'
+#     command: 'node ~{INSTALL_PATH_RELATIVE}/app.js'
+#     path_trim: true
+# requirements:
+#     disk: 160
+
+set -e
+
+VERSION=1.0.3
+
+# https://github.com/actualbudget/actual-server#running
+
+git clone -b v$VERSION --depth 1 https://github.com/actualbudget/actual-server.git $INSTALL_PATH
+yarn install
+cat << EOF > config.json
+{
+    "mode": "production",
+    "port": $PORT,
+    "hostname": "::",
+    "serverFiles": "$INSTALL_PATH/server-files",
+    "userFiles": "$INSTALL_PATH/user-files"
+}
+EOF
