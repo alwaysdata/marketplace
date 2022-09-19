21cb8239991ac4d48037b72c5bbc0c01e05d9367
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-09-19 10:28:34 +0200

    Upgrade Wiki.js to 2.5.288

--- wikijs.sh
+++ wikijs.sh
@@ -14,7 +14,7 @@ set -e
 
 # https://docs.requarks.io/install/linux
 
-wget https://github.com/Requarks/wiki/releases/download/v2.5.287/wiki-js.tar.gz
+wget https://github.com/Requarks/wiki/releases/download/v2.5.288/wiki-js.tar.gz
 
 tar xzf wiki-js.tar.gz -C .
 rm wiki-js.tar.gz
