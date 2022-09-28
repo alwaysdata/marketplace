fe7cbb5f1c9c8af5978fd63cc19b61d12935e467
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-09-28 16:00:08 +0200

    Upgrade Dokuwiki to 2022-07-31a

--- dokuwiki.sh
+++ dokuwiki.sh
@@ -49,7 +49,7 @@ set -e
 
 # https://www.dokuwiki.org/requirements
 
-wget -O- https://github.com/splitbrain/dokuwiki/archive/refs/tags/release_stable_2022-07-31.tar.gz | tar -xz --strip-components=1
+wget -O- https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz | tar -xz --strip-components=1
 
 curl -X POST -F l="$FORM_LANGUAGE" -F d[title]="$FORM_TITLE" -F d[acl]=on -F d[superuser]="$FORM_ADMIN_USERNAME" -F d[fullname]="$FORM_ADMIN_NAME" -F d[email]="$FORM_EMAIL" -F d[password]="$FORM_ADMIN_PASSWORD" -F d[confirm]="$FORM_ADMIN_PASSWORD" -F d[policy]=0 -F d[license]=cc-by-sa -F d[pop]=on -F submit=Save http://$INSTALL_URL/install.php
 
