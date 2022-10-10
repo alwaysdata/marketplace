848c128c2b3b1c22bbe2c334e9bab6bf5ff4aa6d
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-10-10 15:39:52 +0200

    Upgrade Focalboard to 7.4.1

--- focalboard.sh
+++ focalboard.sh
@@ -10,9 +10,10 @@
 
 set -e
 
-wget -O- https://github.com/mattermost/focalboard/releases/download/v7.2.1/focalboard-server-linux-amd64.tar.gz|tar -xz --strip-components=1
+wget -O- https://github.com/mattermost/focalboard/releases/download/v7.4.1/focalboard-server-linux-amd64.tar.gz|tar -xz --strip-components=1
 
 sed -i "s|localhost:8000|$INSTALL_URL|" config.json
-sed -i "/$INSTALL_URL\",/a\ \ \ \ \ \ \ \ \"ip\": \"::\"," config.json
+sed -i "/$INSTALL_URL\",/a\ \ \ \ \"ip\": \"::\"," config.json
 sed -i "s|\"port\": 8000,|\"port\": $PORT,|" config.json
 sed -i "s|/var/tmp/focalboard_local.socket|/home/$USER/admin/tmp/focalboard_local.socket|" config.json
+sed -i "s|\"prometheusaddress\": \":9092\",|\"prometheusaddress\": \"\",|" config.json
