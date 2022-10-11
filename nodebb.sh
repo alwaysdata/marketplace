6b53ee246294f46202f40fa694ccf50142d32a78
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-10-11 14:38:13 +0200

    Upgrade NodeBB to 2.5.4

--- nodebb.sh
+++ nodebb.sh
@@ -31,7 +31,7 @@
 #         max_length: 255
 set -e
 
-wget -O- https://github.com/NodeBB/NodeBB/archive/refs/tags/v2.5.3.tar.gz|tar -xz --strip-components=1
+wget -O- https://github.com/NodeBB/NodeBB/archive/refs/tags/v2.5.4.tar.gz|tar -xz --strip-components=1
 
 cat << EOF > config.json
 {
