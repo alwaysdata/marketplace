b1cf0c07755063819945901ed6c03745c1414bd3
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-10-17 14:14:25 +0200

    Upgrade Leantime to 2.2.8

--- leantime.sh
+++ leantime.sh
@@ -11,7 +11,7 @@
 
 set -e
 
-wget -O- https://github.com/Leantime/leantime/releases/download/v2.2.7/Leantime-v2.2.7.tar.gz | tar -xz --strip-components=1
+wget -O- https://github.com/Leantime/leantime/releases/download/v2.2.8/Leantime-v2.2.8.tar.gz | tar -xz --strip-components=1
 
 cp config/configuration.sample.php config/configuration.php
 
