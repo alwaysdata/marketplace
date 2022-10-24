5aa343930c0e1be654e8e7683a8628fc5111b1f3
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-10-24 17:01:49 +0200

    Upgrade phpList to 3.6.10

--- phplist.sh
+++ phplist.sh
@@ -25,27 +25,27 @@ set -e
 
 # https://www.phplist.org/manual/books/phplist-manual/page/installing-phplist-manually
 
-wget -O- https://sourceforge.net/projects/phplist/files/phplist/3.6.9/phplist-3.6.9.tgz/download | tar -xz --strip-components=0
+wget -O- https://sourceforge.net/projects/phplist/files/phplist/3.6.10/phplist-3.6.10.tgz | tar -xz --strip-components=0
 
-sed -i "s|database_host = 'localhost';|database_host = '$DATABASE_HOST';|" phplist-3.6.9/public_html/lists/config/config.php
-sed -i "s|database_name = 'phplistdb';|database_name = '$DATABASE_NAME';|" phplist-3.6.9/public_html/lists/config/config.php
-sed -i "s|database_user = 'phplist';|database_user = '$DATABASE_USERNAME';|" phplist-3.6.9/public_html/lists/config/config.php
-sed -i "s|database_password = 'phplist';|database_password = '$DATABASE_PASSWORD';|" phplist-3.6.9/public_html/lists/config/config.php
-sed -i "s|define('PHPMAILERHOST', 'localhost');|define('PHPMAILERHOST', 'smtp-$USER.$RESELLER_DOMAIN');|" phplist-3.6.9/public_html/lists/config/config.php
-sed -i "s|listbounces@yourdomain|$USER@$RESELLER_DOMAIN|" phplist-3.6.9/public_html/lists/config/config.php
-sed -i "s|/var/mail/listbounces|$INSTALL_PATH/var/mail/listbounces|" phplist-3.6.9/public_html/lists/config/config.php
+sed -i "s|database_host = 'localhost';|database_host = '$DATABASE_HOST';|" phplist-3.6.10/public_html/lists/config/config.php
+sed -i "s|database_name = 'phplistdb';|database_name = '$DATABASE_NAME';|" phplist-3.6.10/public_html/lists/config/config.php
+sed -i "s|database_user = 'phplist';|database_user = '$DATABASE_USERNAME';|" phplist-3.6.10/public_html/lists/config/config.php
+sed -i "s|database_password = 'phplist';|database_password = '$DATABASE_PASSWORD';|" phplist-3.6.10/public_html/lists/config/config.php
+sed -i "s|define('PHPMAILERHOST', 'localhost');|define('PHPMAILERHOST', 'smtp-$USER.$RESELLER_DOMAIN');|" phplist-3.6.10/public_html/lists/config/config.php
+sed -i "s|listbounces@yourdomain|$USER@$RESELLER_DOMAIN|" phplist-3.6.10/public_html/lists/config/config.php
+sed -i "s|/var/mail/listbounces|$INSTALL_PATH/var/mail/listbounces|" phplist-3.6.10/public_html/lists/config/config.php
 
-sed -i "s|pageroot = '/lists';|pageroot = '$INSTALL_URL_PATH';|" phplist-3.6.9/public_html/lists/config/config_extended.php
+sed -i "s|pageroot = '/lists';|pageroot = '$INSTALL_URL_PATH';|" phplist-3.6.10/public_html/lists/config/config_extended.php
 
 if [ "$INSTALL_URL_PATH" = "/" ]
 then
-sed -i "s|pageroot = '/';|pageroot = '';|" phplist-3.6.9/public_html/lists/config/config_extended.php
+sed -i "s|pageroot = '/';|pageroot = '';|" phplist-3.6.10/public_html/lists/config/config_extended.php
 fi
 
-ADMIN_PASSWORD="$FORM_ADMIN_PASSWORD" ADMIN_EMAIL="$FORM_ADMIN_EMAIL" php phplist-3.6.9/public_html/lists/admin/index.php -p initialise -c $INSTALL_PATH/phplist-3.6.9/public_html/lists/config/config.php
+ADMIN_PASSWORD="$FORM_ADMIN_PASSWORD" ADMIN_EMAIL="$FORM_ADMIN_EMAIL" php phplist-3.6.10/public_html/lists/admin/index.php -p initialise -c $INSTALL_PATH/phplist-3.6.10/public_html/lists/config/config.php
 
 shopt -s dotglob
-mv phplist-3.6.9/public_html/lists/* .
-rm -rf  .wget-hsts phplist-3.6.9
+mv phplist-3.6.10/public_html/lists/* .
+rm -rf  .wget-hsts phplist-3.6.10
 
 # default username: admin
