3a69b1b656c0d487288c390b347f6c5f99a64672
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-10-24 15:45:11 +0200

    Upgrade MyTinyTodo to 1.7.0

--- mytinytodo.sh
+++ mytinytodo.sh
@@ -8,32 +8,29 @@
 #     type: mysql
 # requirements:
 #     disk: 5
-# form:
-#     language:
-#         type: choices
-#         label:
-#             en: Language
-#             fr: Langue
-#         choices:
-#             de: Deutsch
-#             en: English
-#             es: Español
-#             fr: Français
-#             it: Italiano
 
 set -e
 
-wget -O- https://github.com/maxpozdeev/mytinytodo/releases/download/v1.6.10/mytinytodo-v1.6.10.zip | bsdtar --strip-components=1 -xf -
+wget -O- https://github.com/maxpozdeev/mytinytodo/releases/download/v1.7.0/mytinytodo-v1.7.0.tar.gz| tar -xz --strip-components=1
 
-sed -i "s|\['db'\] = '';|\['db'\] = 'mysql';|" db/config.php
-sed -i "s|localhost|$DATABASE_HOST|" db/config.php
-sed -i "s|\['mysql.db'\] = \"mytinytodo\";|\['mysql.db'\] = '$DATABASE_NAME';|" db/config.php
-sed -i "s|\['mysql.user'\] = \"user\"|\['mysql.user'\] = '$DATABASE_USERNAME';|" db/config.php
-sed -i "s|\['mysql.password'\] = \"\";|\['mysql.password'\] = '$DATABASE_PASSWORD';|" db/config.php
-sed -i "s|\['lang'\] = \"en\"|\['lang'\] = \"$FORM_LANGUAGE\"|" db/config.php
-sed -i "s|\['timezone'\] = 'UTC'|\['timezone'\] = 'Europe/Paris'|" db/config.php
+cat << EOF > config.php
+<?php
 
-curl -X POST -F installdb=mysql -F mysql_host='$DATABASE_HOST' -F mysql-db='$DATABASE_NAME' -F mysql_user='$DATABASE_USERNAME' -F mysql_password='$DATABASE_PASSWORD' -F submit=Next https://$INSTALL_URL/setup.php
-curl -X POST -F install=Install https://$INSTALL_URL/setup.php
+define("MTT_DB_TYPE", "mysql");
+
+define("MTT_DB_HOST", "$DATABASE_HOST");
+
+define("MTT_DB_NAME", "$DATABASE_NAME");
+
+define("MTT_DB_USER", "$DATABASE_USERNAME");
+
+define("MTT_DB_PASSWORD", "$DATABASE_PASSWORD");
+
+define("MTT_DB_PREFIX", "");
+
+define("MTT_SALT", "$(echo $RANDOM | md5sum | sed 's/ .*//')");
+EOF
+
+curl -X POST -F install=create https://$INSTALL_URL/setup.php
   
 rm setup.php
