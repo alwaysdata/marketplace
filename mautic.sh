b60c4d12e512d9b3e4f9cdd5a70ce3433a3b4b52
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-10-26 10:14:27 +0200

    Upgrade Mautic to 4.4.4

--- mautic.sh
+++ mautic.sh
@@ -8,12 +8,13 @@
 #         memory_limit = 4096M
 #         extension = imap.so
 #         extension = intl.so
+#         extension = sockets.so
 #         zend.assertions = -1
 #     ssl_force: true
 # database:
 #     type: mysql
 # requirements:
-#     disk: 300
+#     disk: 400
 # form:
 #     email:
 #         type: email
@@ -35,8 +36,14 @@
 
 set -e
 
-wget -O- https://github.com/mautic/mautic/releases/download/4.4.3/4.4.3.zip | bsdtar --strip-components=0 -xf -
+COMPOSER_CACHE_DIR=/dev/null composer2 create-project mautic/core
+
+cd core
 
 php bin/console mautic:install --db_driver="pdo_mysql" --db_host="$DATABASE_HOST" --db_port="3306" --db_name="$DATABASE_NAME" --db_user="$DATABASE_USERNAME" --db_password="$DATABASE_PASSWORD" --admin_username="$FORM_ADMIN_USERNAME" --admin_email="$FORM_EMAIL" --admin_password="$FORM_ADMIN_PASSWORD" --mailer_from_email="$USER@$RESELLER_DOMAIN" --mailer_from_name="Mautic" https://$INSTALL_URL
 
-rm .wget-hsts
+cd
+rm -rf .wget-hsts .config .local .subversion vendor
+shopt -s dotglob
+mv core/* .
+rmdir core
