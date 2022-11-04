43ea96286f097052837a39985d5c03905d38182d
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-11-04 11:23:55 +0100

    Adding Sanic framework

---
+++ sanic.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# site:
+#     type: user_program
+#     working_directory: '{INSTALL_PATH}'
+#     command: 'env/bin/sanic hello.app --host="::" --port=$PORT'
+#     environment: |
+#         PYTHON_VERSION=3.10
+#     path_trim: true
+#     ssl_force: true
+# requirements:
+#     disk: 50
+
+set -e
+
+export PYTHON_VERSION=3.10
+
+python -m venv env
+source env/bin/activate
+
+# https://sanic.dev/en/guide/getting-started.html
+
+pip --cache-dir=/dev/null install sanic
+
+cat << EOF > hello.py
+from sanic import Sanic
+from sanic.response import text
+
+app = Sanic(__name__)
+
+@app.get("/")
+async def hello(request):
+    return text("Hello, world.")
+EOF
