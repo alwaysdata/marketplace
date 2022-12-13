43559e0efcf0997a8201cafb76d13cae1a7d5715
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-12-13 14:30:42 +0100

    Symfony: skeleton change as website-skeleton is no longer maintain

--- symfony.sh
+++ symfony.sh
@@ -3,16 +3,19 @@
 # site:
 #     type: php
 #     path: '{INSTALL_PATH_RELATIVE}/public/'
-#     php_version: '8.1'
+#     php_version: '8.2'
 # requirements:
-#     disk: 80
+#     disk: 100
 
 set -e
 
 # https://symfony.com/doc/current/setup.html
-COMPOSER_CACHE_DIR=/dev/null composer2 create-project symfony/website-skeleton default
+COMPOSER_CACHE_DIR=/dev/null composer2 create-project symfony/skeleton default
+cd default
+echo "n"|COMPOSER_CACHE_DIR=/dev/null composer2 require webapp
 
-rm -rf .composer
+cd
+rm -rf .config .local .subversion
 
 shopt -s dotglob
 mv default/* .
