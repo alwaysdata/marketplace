75f7f65fbf027af7ebec8bb1800f4615aa08e25d
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2022-12-14 09:36:36 +0100

    Upgrade MediaWiki to 1.39.0

--- mediawiki.sh
+++ mediawiki.sh
@@ -3,7 +3,7 @@
 # site:
 #     type: php
 #     path: '{INSTALL_PATH_RELATIVE}'
-#     php_version: '7.4'
+#     php_version: '8.1'
 #     php_ini: extension=intl.so
 # database:
 #     type: mysql
@@ -43,9 +43,9 @@ set -e
 
 # https://www.mediawiki.org/wiki/Compatibility
 
-wget -O- https://releases.wikimedia.org/mediawiki/1.38/mediawiki-1.38.4.tar.gz | tar -xz --strip-components=1
+wget -O- https://releases.wikimedia.org/mediawiki/1.39/mediawiki-1.39.0.tar.gz | tar -xz --strip-components=1
 
-COMPOSER_CACHE_DIR=/dev/null composer install
+COMPOSER_CACHE_DIR=/dev/null composer2 install
 php maintenance/install.php --dbname="$DATABASE_NAME" --installdbpass="$DATABASE_PASSWORD" --dbserver="$DATABASE_HOST" --installdbuser="$DATABASE_USERNAME" --dbuser="$DATABASE_USERNAME" --dbpass="$DATABASE_PASSWORD" --dbprefix=wiki --lang="$FORM_LANGUAGE" --pass="$FORM_ADMIN_PASSWORD" --server="http://$INSTALL_URL_HOSTNAME" --scriptpath="$INSTALL_URL_PATH" --skins=Vector "$FORM_TITLE" "$FORM_ADMIN_USERNAME"
 
 if [ "$INSTALL_URL_PATH" = "/" ]
