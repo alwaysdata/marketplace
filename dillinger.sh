f49e6e7e6ebc732b7e092631fc457a2993e5def8
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-02-22 12:25:16 +0100

    Dillinger: use NodeJS 16.17 + fix script

--- dillinger.sh
+++ dillinger.sh
@@ -2,14 +2,14 @@
 
 # site:
 #     type: nodejs
-#     nodejs_version: '16'
+#     nodejs_version: '16.17'
 #     working_directory: '{INSTALL_PATH}'
 #     command: 'node app.js'
 #     environment: 'NODE_ENV=production'
 # requirements:
-#     disk: 680
+#     disk: 850
 
 set -e
 
 git clone https://github.com/joemccann/dillinger.git .
-echo y | npm install --production
+npm install --omit=dev
