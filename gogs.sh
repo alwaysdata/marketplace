65d025ffc13f444cb6b197c4063af616ecdb440b
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-02-27 11:37:05 +0100

    Upgrade Gogs to 0.13.0

--- gogs.sh
+++ gogs.sh
@@ -12,7 +12,7 @@
 
 set -e
 
-wget -O- https://github.com/gogs/gogs/releases/download/v0.12.10/gogs_0.12.10_linux_amd64.tar.gz|tar -xz --strip-components=1
+wget -O- https://github.com/gogs/gogs/releases/download/v0.13.0/gogs_0.13.0_linux_amd64.tar.gz|tar -xz --strip-components=1
 
 mkdir -p custom/conf
 
@@ -25,4 +25,4 @@ DOMAIN = 0.0.0.0
 EXTERNAL_URL = http://$INSTALL_URL/
 EOF
 
-curl -X POST -F db_type=PostgreSQL -F db_host=$DATABASE_HOST:5432 -F db_user=$DATABASE_USERNAME -F db_passwd=$DATABASE_PASSWORD -F db_name=$DATABASE_NAME -F app_name=Gogs -F repo_root_path=$INSTALL_PATH/gogs-repositories -F run_user=$USER -F domain=0.0.0.0 -F http_port=$PORT -F app_url=http://$INSTALL_URL/ -F log_root_path=$INSTALL_PATH/log -F submit="Installer Gogs" http://$INSTALL_URL/install
+curl -X POST -F db_type=PostgreSQL -F db_host=$DATABASE_HOST:5432 -F db_user=$DATABASE_USERNAME -F db_passwd=$DATABASE_PASSWORD -F db_name=$DATABASE_NAME -F db_schema=public -F app_name=Gogs -F repo_root_path=$INSTALL_PATH/gogs-repositories -F run_user=$USER -F domain=0.0.0.0 -F http_port=$PORT -F app_url=http://$INSTALL_URL/ -F log_root_path=$INSTALL_PATH/log -F submit="Installer Gogs" http://$INSTALL_URL/install
