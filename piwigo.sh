8b5b19e0fd330710859ee91a284a2a821c2dae4d
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-02-27 11:37:29 +0100

    Upgrade Piwigo to 13.6.0

--- piwigo.sh
+++ piwigo.sh
@@ -29,6 +29,6 @@
 
 set -e
 
-wget -O- https://github.com/Piwigo/Piwigo/archive/refs/tags/13.5.0.tar.gz| tar -xz --strip-components=1
+wget -O- https://github.com/Piwigo/Piwigo/archive/refs/tags/13.6.0.tar.gz| tar -xz --strip-components=1
 
 curl -X POST -F language="en_US" -F dbhost="$DATABASE_HOST" -F dbuser="$DATABASE_USERNAME" -F dbpasswd="$DATABASE_PASSWORD" -F dbname="$DATABASE_NAME" -F admin_name="$FORM_USERNAME" -F admin_pass1="$FORM_PASSWORD" -F admin_pass2="$FORM_PASSWORD" -F admin_mail="$FORM_EMAIL" -F newsletter_subscribe=none -F send_credentials_by_mail=checked -F install=Start\ installation http://$INSTALL_URL/install.php
