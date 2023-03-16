0edfe63b4129884bf4fd28e36f8ab3de7c8ae472
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-03-16 13:42:08 +0100

    Upgrade Microweber to 1.3.3

--- microweber.sh
+++ microweber.sh
@@ -12,7 +12,7 @@
 # database:
 #     type: mysql
 # requirements:
-#     disk: 400
+#     disk: 500
 # form:
 #     email:
 #         type: email
@@ -35,13 +35,46 @@ set -e
 
 ad_install_pecl mcrypt
 
-COMPOSER_CACHE_DIR=/dev/null composer2 create-project microweber/microweber=v1.3.2 --no-interaction
+COMPOSER_CACHE_DIR=/dev/null composer2 create-project microweber/microweber --no-interaction
 
 cd microweber
-echo "yes" | php artisan key:generate
 
-# http://docs.microweber.com/guides/installation_cli.md
-php artisan microweber:install --email=$FORM_EMAIL --username=$FORM_ADMIN_USERNAME --password=$FORM_ADMIN_PASSWORD --db-host=$DATABASE_HOST --db-name=$DATABASE_NAME --db-username=$DATABASE_USERNAME --db-password=$DATABASE_PASSWORD --db-driver=mysql --template=default --default-content
+# To avoid "Error: Could not connect to the database.  Please check your configuration. SQLSTATE[HY000] [2002] No such file or directory" we prepare the database connection file
+mv config/database.php config/database.php.example
+cat << EOF > config/database.php
+<?php return array (
+  'fetch' => 8,
+  'default' => 'mysql',
+  'connections' =>
+  array (
+    'mysql' =>
+    array (
+      'driver' => 'mysql',
+      'host'      => '$DATABASE_HOST',
+      'database'  => '$DATABASE_NAME',
+      'username'  => '$DATABASE_USERNAME',
+      'password'  => '$DATABASE_PASSWORD',
+      'charset' => 'utf8',
+      'collation' => 'utf8_unicode_ci',
+      'prefix' => '',
+      'strict' => false,
+    ),
+  ),
+  'migrations' => 'migrations',
+  'redis' =>
+  array (
+    'cluster' => false,
+    'default' =>
+    array (
+      'host' => '127.0.0.1',
+      'port' => 6379,
+      'database' => 0,
+    ),
+  ),
+);
+EOF
+
+php artisan microweber:install --email="$FORM_EMAIL" --username="$FORM_ADMIN_USERNAME" --password="$FORM_ADMIN_PASSWORD" --db-host="$DATABASE_HOST" --db-name="$DATABASE_NAME" --db-username="$DATABASE_USERNAME" --db-password="$DATABASE_PASSWORD" --db-driver=mysql --template=default --default-content
 
 # Nettoyage
 cd
