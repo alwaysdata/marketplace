e26f7e98fdd1e55b1721c4cbd6c726f03e6024e5
m4dz <code@m4dz.net>
2023-03-17 15:24:12 +0100

    nextjs: improve install and starting command line

--- nextjs.sh
+++ nextjs.sh
@@ -4,31 +4,27 @@
 #     type: nodejs
 #     nodejs_version: '18'
 #     working_directory: '{INSTALL_PATH}'
-#     command: 'npx next start -H ::'
-#     environment: HOME={INSTALL_PATH}
+#     command: 'npx next start --port $PORT --hostname $IP'
 # requirements:
-#     disk: 340
+#     disk: 360
 
 set -e
 
-npm install yarn
-$HOME/node_modules/yarn/bin/yarn create next-app --experimental-app --typescript --eslint --src-dir project
+export NPM_CONFIG_CACHE=$(mktemp -d)
+export XDG_CONFIG_HOME=$(mktemp -d)
 
-cd project
+npm create -y --force next-app -- \
+    --use-npm \
+    --experimental-app \
+    --import-alias='@/*' \
+    --typescript \
+    --eslint \
+    --src-dir $INSTALL_PATH
 
 if [ "$INSTALL_URL_PATH" != "/" ]
 then
-echo "
-module.exports = {
-  basePath: '$INSTALL_URL_PATH',
-}" >> next.config.js
+    sed -i '/nextConfig = {$/ a \ \ basePath: "'"$INSTALL_URL_PATH"'",' next.config.js
+    sed -i 's,src=",src="'"$INSTALL_URL_PATH"',g' src/app/page.tsx
 fi
 
 npx next build
-
-cd
-rm -rf .yarn* package* .npm node_modules .config .cache
-
-shopt -s dotglob
-mv project/* .
-rmdir project
