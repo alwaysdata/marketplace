8a51fa2bcd94f319ddd499885d48ef792f6854b5
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-03-21 14:57:12 +0100

    Upgrade Shaarli to 0.12.2

--- shaarli.sh
+++ shaarli.sh
@@ -7,6 +7,11 @@
 # requirements:
 #     disk: 20
 # form:
+#     title:
+#         label:
+#             en: Title
+#             fr: Titre
+#         max_length: 255
 #     language:
 #         type: choices
 #         label:
@@ -28,13 +33,8 @@
 #             fr: Mot de passe
 #         max_length: 255
 #         min_length: 8
-#     title:
-#         label:
-#             en: Title
-#             fr: Titre
-#         max_length: 255
 set -e
 
-wget -O- https://github.com/shaarli/Shaarli/releases/download/v0.12.1/shaarli-v0.12.1-full.tar.gz | tar -xz --strip-components=1
+wget -O- https://github.com/shaarli/Shaarli/releases/download/v0.12.2/shaarli-v0.12.2-full.tar.gz | tar -xz --strip-components=1
 
 curl -X POST -F setlogin="$FORM_USERNAME" -F setpassword="$FORM_PASSWORD" -F title="$FORM_TITLE" -F language="$FORM_LANGUAGE" -F Save=Install http://$INSTALL_URL/install
