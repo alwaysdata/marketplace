af605419a0a12b4b833b3f8d0a4c78120aae4f50
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-04-04 11:41:21 +0200

    Focalboard to 7.9.3

--- focalboard.sh
+++ focalboard.sh
@@ -12,7 +12,7 @@
 
 set -e
 
-wget -O- https://github.com/mattermost/focalboard/releases/download/v7.9.2/focalboard-server-linux-amd64.tar.gz|tar -xz --strip-components=1
+wget -O- https://github.com/mattermost/focalboard/releases/download/v7.9.3/focalboard-server-linux-amd64.tar.gz|tar -xz --strip-components=1
 
 mv config.json config.json.example
 cat << EOF > config.json
