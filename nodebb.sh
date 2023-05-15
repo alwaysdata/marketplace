bde0be7f1bbacf0dbf9afb92d09be2dd517ce79b
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-05-15 11:08:07 +0200

    Upgrade NodeBB to 3.1.2

--- nodebb.sh
+++ nodebb.sh
@@ -31,7 +31,7 @@
 #         max_length: 255
 set -e
 
-wget -O- https://github.com/NodeBB/NodeBB/archive/refs/tags/v3.1.0.tar.gz|tar -xz --strip-components=1
+wget -O- https://github.com/NodeBB/NodeBB/archive/refs/tags/v3.1.2.tar.gz|tar -xz --strip-components=1
 
 cat << EOF > config.json
 {
