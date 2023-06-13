5ff1cab4b509bbf0fcb6b598a38c0d5bc0e4cfbc
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-06-13 15:10:01 +0200

    Upgrade Leantime to 2.3.25

--- leantime.sh
+++ leantime.sh
@@ -11,7 +11,7 @@
 
 set -e
 
-wget -O- https://github.com/Leantime/leantime/releases/download/v2.3.23/Leantime-v2.3.23.tar.gz | tar -xz --strip-components=1
+wget -O- https://github.com/Leantime/leantime/releases/download/v2.3.25/Leantime-v2.3.25.tar.gz | tar -xz --strip-components=1
 
 cp config/configuration.sample.php config/configuration.php
 
