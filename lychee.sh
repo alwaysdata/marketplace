b7e25797285274a9444192d85eddc02807c333d4
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-06-29 10:58:35 +0200

    Upgrade Lychee to 4.9.4

--- lychee.sh
+++ lychee.sh
@@ -12,7 +12,7 @@
 
 set -e
 
-wget -O- https://github.com/LycheeOrg/Lychee/releases/download/v4.9.3/Lychee.zip | bsdtar --strip-components=1 -xf -
+wget -O- https://github.com/LycheeOrg/Lychee/releases/download/v4.9.4/Lychee.zip | bsdtar --strip-components=1 -xf -
 
 sed -i "s|http://localhost|http://$INSTALL_URL|" .env.example
 sed -i "s|DB_CONNECTION=sqlite|DB_CONNECTION=mysql|" .env.example
