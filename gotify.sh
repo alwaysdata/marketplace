d72d96122029598445f8f041ece21c6ddcaeb41b
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-07-25 10:58:50 +0200

    Upgrade Gotify to 2.3.0

--- gotify.sh
+++ gotify.sh
@@ -9,7 +9,7 @@
 
 set -e
 
-wget -O- https://github.com/gotify/server/releases/download/v2.2.5/gotify-linux-amd64.zip | bsdtar -xf -
+wget -O- https://github.com/gotify/server/releases/download/v2.3.0/gotify-linux-amd64.zip | bsdtar -xf -
 chmod +x gotify-linux-amd64
 
 wget -O config.yml https://raw.githubusercontent.com/gotify/server/master/config.example.yml
