fdb89bbab352aad0aff169ad3fa3cc98c88f0227
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-08-07 11:53:42 +0200

    Upgrade Omeka S to 4.0.3

--- omeka-s.sh
+++ omeka-s.sh
@@ -45,7 +45,7 @@
 
 set -e
 
-wget -O- https://github.com/omeka/omeka-s/archive/v4.0.2.tar.gz | tar -xz --strip-components=1
+wget -O- https://github.com/omeka/omeka-s/archive/v4.0.3.tar.gz | tar -xz --strip-components=1
 
 # https://omeka.org/s/docs/user-manual/install/
 
