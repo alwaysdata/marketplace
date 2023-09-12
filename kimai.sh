14a45752c47d689fd8604e109df23985d1141ae1
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-09-12 11:51:51 +0200

    Upgrade Kimai to 2.0.33

--- kimai.sh
+++ kimai.sh
@@ -29,7 +29,7 @@
 
 set -e
 
-git clone -b 2.0.32 --depth 1 https://github.com/kimai/kimai.git
+git clone -b 2.0.33 --depth 1 https://github.com/kimai/kimai.git
 cd kimai/
 
 COMPOSER_CACHE_DIR=/dev/null composer2 install --no-dev --optimize-autoloader -n
