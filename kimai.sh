ab78b0c26241c26ac2dfcaeca6de31519ee1b275
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-10-11 11:55:01 +0200

    Upgrade Kimai to 2.0.35

--- kimai.sh
+++ kimai.sh
@@ -29,7 +29,7 @@
 
 set -e
 
-git clone -b 2.0.34 --depth 1 https://github.com/kimai/kimai.git
+git clone -b 2.0.35 --depth 1 https://github.com/kimai/kimai.git
 cd kimai/
 
 COMPOSER_CACHE_DIR=/dev/null composer2 install --no-dev --optimize-autoloader -n
