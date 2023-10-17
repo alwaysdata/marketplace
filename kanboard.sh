df04ae6c6a5e61404fe003adeb3d371e3337c2b4
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-10-17 12:22:52 +0200

    Upgrade Kanboard to 1.2.33

--- kanboard.sh
+++ kanboard.sh
@@ -11,7 +11,7 @@
 
 set -e
 
-wget -O- https://github.com/kanboard/kanboard/archive/refs/tags/v1.2.32.tar.gz | tar -xz --strip-components=1
+wget -O- https://github.com/kanboard/kanboard/archive/refs/tags/v1.2.33.tar.gz | tar -xz --strip-components=1
 
 # https://docs.kanboard.org/en/latest/admin_guide/config_file.html
 mv config.default.php config.php
