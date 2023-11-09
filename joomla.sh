2cf9a585ad626ce3e265065e89ea6d3080b82440
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-11-09 15:27:41 +0100

    Joomla: set version as last version does not work

--- joomla.sh
+++ joomla.sh
@@ -22,7 +22,7 @@ set -e
 
 COMPOSER_CACHE_DIR=/dev/null composer2 global require joomlatools/console
 
-php .config/composer/vendor/bin/joomla site:download --www="$INSTALL_PATH" default
+php .config/composer/vendor/bin/joomla site:download --www="$INSTALL_PATH" --release=4.4.0 default
 php .config/composer/vendor/bin/joomla site:install --www="$INSTALL_PATH" --mysql-login="$DATABASE_USERNAME":"$DATABASE_PASSWORD" --mysql-host="$DATABASE_HOST" --mysql-database="$DATABASE_NAME" --skip-exists-check --drop default
 
 sed -i "s|'default'|'$FORM_TITLE'|" default/configuration.php
