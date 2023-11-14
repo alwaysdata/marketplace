c66428911b2477ff8523cd6d4596f9fca3954077
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-11-14 12:00:16 +0100

    Adding Forgejo

---
+++ forgejo.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# site:
+#     type: user_program
+#     working_directory: '{INSTALL_PATH}'
+#     command: './forgejo web'
+#     path_trim: true
+# database:
+#     type: postgresql
+# requirements:
+#     disk: 110
+
+set -e
+
+wget https://codeberg.org/forgejo/forgejo/releases/download/v1.20.5-0/forgejo-1.20.5-0-linux-amd64
+
+ln -s  forgejo-1.20.5-0-linux-amd64 forgejo
+chmod +x  forgejo
+
+# https://forgejo.org/docs/latest/admin/config-cheat-sheet/
+mkdir -p custom/conf
+cat << EOF > custom/conf/app.ini
+[server]
+DOMAIN = $INSTALL_URL
+HTTP_ADDR = "::"
+HTTP_PORT = $PORT
+
+[database]
+DB_TYPE = postgres
+HOST = $DATABASE_HOST
+NAME = $DATABASE_NAME
+USER = $DATABASE_USERNAME
+PASSWD = $DATABASE_PASSWORD
+EOF
+
+rm .wget-hsts
+
+# After is GUI
