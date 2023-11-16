2c7afc21b69d9c16518fb7cdb780299467b3a6af
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2023-11-16 11:38:55 +0100

    Upgrade Gitea to 1.21.0

--- gitea.sh
+++ gitea.sh
@@ -15,7 +15,7 @@ set -e
 
 # https://docs.gitea.io/en-us/install-from-binary/
 
-wget -O gitea https://github.com/go-gitea/gitea/releases/download/v1.20.5/gitea-1.20.5-linux-amd64
+wget -O gitea https://github.com/go-gitea/gitea/releases/download/v1.21.0/gitea-1.21.0-linux-amd64
 chmod +x gitea
 
 mkdir -p custom/conf data indexers public log
