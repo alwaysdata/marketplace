ab9397ccc93c798f458411590d8ff146432d7529
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2024-01-04 14:02:44 +0100

    Upgrade MediaWiki to 1.41.0

--- mediawiki.sh
+++ mediawiki.sh
@@ -43,7 +43,7 @@ set -e
 
 # https://www.mediawiki.org/wiki/Compatibility
 
-wget -O- https://releases.wikimedia.org/mediawiki/1.40/mediawiki-1.40.1.tar.gz | tar -xz --strip-components=1
+wget -O- https://releases.wikimedia.org/mediawiki/1.41/mediawiki-1.41.0.tar.gz | tar -xz --strip-components=1
 
 COMPOSER_CACHE_DIR=/dev/null composer2 install
 php maintenance/install.php --dbname="$DATABASE_NAME" --installdbpass="$DATABASE_PASSWORD" --dbserver="$DATABASE_HOST" --installdbuser="$DATABASE_USERNAME" --dbuser="$DATABASE_USERNAME" --dbpass="$DATABASE_PASSWORD" --dbprefix=wiki --lang="$FORM_LANGUAGE" --pass="$FORM_ADMIN_PASSWORD" --server="http://$INSTALL_URL_HOSTNAME" --scriptpath="$INSTALL_URL_PATH" --skins=Vector "$FORM_TITLE" "$FORM_ADMIN_USERNAME"
