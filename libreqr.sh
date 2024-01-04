e74b7172adbf509f483f68d6164dcf81da159d07
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2024-01-04 12:03:49 +0100

    Upgrade LibreQR to 2.0.1

--- libreqr.sh
+++ libreqr.sh
@@ -3,10 +3,10 @@
 # site:
 #     type: php
 #     path: '{INSTALL_PATH_RELATIVE}'
-#     php_version: '7.4'
+#     php_version: '8.3'
 # requirements:
 #     disk: 25
 
 set -e
 
-wget -O- https://code.antopie.org/miraty/libreqr/archive/1.3.0.zip | bsdtar --strip-components=1 -xf -
+wget -O- https://code.antopie.org/miraty/libreqr/archive/2.0.1.zip | bsdtar --strip-components=1 -xf -
