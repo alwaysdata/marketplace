d26d8d0212d13074ee2e26d98e9dff9486c59e09
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2024-01-15 14:50:40 +0100

    Jenkins: set environment variable

--- jenkins.sh
+++ jenkins.sh
@@ -4,10 +4,13 @@
 #     type: user_program
 #     working_directory: '{INSTALL_PATH}'
 #     command: "java17 -Xmx512m -jar jenkins.war --httpListenAddress=0.0.0.0 --httpPort=$PORT"
+#     environment: |
+#          JENKINS_HOME={INSTALL_PATH}
 # requirements:
 #     disk: 75
 
 set -e
 
 wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
+rm .wget-hsts
 # The rest is GUI - Jenkins blocks POST requests which we need to continue (via the cURL command)
