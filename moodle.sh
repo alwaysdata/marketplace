8556f65963a42b86d00b78833a8f2354b5922a9e
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2024-01-22 11:51:25 +0100

    Moodle: update regex to allow more non-alphanumeric characters

--- moodle.sh
+++ moodle.sh
@@ -44,7 +44,7 @@
 #         label:
 #             en: Administrator password
 #             fr: Mot de passe de l'administrateur
-#         regex: ^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[*\-#])[A-Za-z\d*\-#]{8,}$
+#         regex: ^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[*-+_!@#$%^&.,?])[A-Za-z\d*-+_!@#$%^&.,?]{8,}$
 #         min_length: 8
 #         max_length: 255
 
