80a1b0537ce5ba9e91cbf094a92db1837ba03b14
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2024-02-26 10:45:49 +0100

    Upgrade Kimai to 2.12.0

--- kimai.sh
+++ kimai.sh
@@ -31,7 +31,7 @@
 set -e
 
 # Download and install dependancies
-git clone -b 2.11.0 --depth 1 https://github.com/kimai/kimai.git
+git clone -b 2.12.0 --depth 1 https://github.com/kimai/kimai.git
 cd kimai/
 
 COMPOSER_CACHE_DIR=/dev/null composer2 install --no-dev --optimize-autoloader -n
