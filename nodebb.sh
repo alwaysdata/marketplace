3b2d095f338f505ff96a3c7f1421a34cd1875480
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2024-04-05 16:15:25 +0200

    NodeBB: use Node 20.9 + Upgrade to 3.7.3
    With NodeBB 20.11 we have Installation error: connect ECONNREFUSED 140.82.121.3:443

--- nodebb.sh
+++ nodebb.sh
@@ -3,7 +3,7 @@
 # Declare site in YAML, as documented on the documentation: https://help.alwaysdata.com/en/marketplace/build-application-script/
 # site:
 #     type: nodejs
-#     nodejs_version: '20'
+#     nodejs_version: '20.9'
 #     working_directory: '{INSTALL_PATH}'
 #     command: 'node app.js'
 #     environment: |
@@ -35,7 +35,7 @@ set -e
 # https://docs.nodebb.org/installing/os/ubuntu/#installing-nodebb
 
 # Download
-wget -O- --no-hsts https://github.com/NodeBB/NodeBB/archive/refs/tags/v3.7.0.tar.gz|tar -xz --strip-components=1
+wget -O- --no-hsts https://github.com/NodeBB/NodeBB/archive/refs/tags/v3.7.3.tar.gz|tar -xz --strip-components=1
 
 # Configuration
 cat << EOF > config.json
