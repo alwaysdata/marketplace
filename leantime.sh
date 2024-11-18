e14df3efe6a7eca41d3b8f32868c0d4ba733aed6
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2024-11-18 11:53:17 +0100

    Upgrade Leantime to 3.3.0

--- leantime.sh
+++ leantime.sh
@@ -3,7 +3,7 @@
 # Declare site in YAML, as documented on the documentation: https://help.alwaysdata.com/en/marketplace/build-application-script/
 # site:
 #     type: php
-#     path: '{INSTALL_PATH_RELATIVE}'
+#     path: '{INSTALL_PATH_RELATIVE}/public'
 #     php_version: '8.3'
 # database:
 #     type: mysql
@@ -13,19 +13,19 @@
 set -e
 
 # Download
-wget -O- --no-hsts https://github.com/Leantime/leantime/releases/download/v3.2.1/Leantime-v3.2.1.tar.gz | tar -xz --strip-components=1
+wget -O- --no-hsts https://github.com/Leantime/leantime/releases/download/v3.3.0/Leantime-v3.3.0.tar.gz | tar -xz --strip-components=1
 
 # Configuration
-cp config/configuration.sample.php config/configuration.php
+cp config/sample.env config/.env
 
-sed -i "s|dbHost = 'localhost'|dbHost = '$DATABASE_HOST'|" config/configuration.php
-sed -i "s|dbUser = ''|dbUser = '$DATABASE_USERNAME'|" config/configuration.php
-sed -i "s|dbPassword = ''|dbPassword = '$DATABASE_PASSWORD'|" config/configuration.php
-sed -i "s|dbDatabase = ''|dbDatabase = '$DATABASE_NAME'|" config/configuration.php
+sed -i "s|LEAN_DB_HOST = 'localhost'|LEAN_DB_HOST = '$DATABASE_HOST'|" config/.env
+sed -i "s|LEAN_DB_USER = ''|LEAN_DB_USER = '$DATABASE_USERNAME'|" config/.env
+sed -i "s|LEAN_DB_PASSWORD = ''|LEAN_DB_PASSWORD = '$DATABASE_PASSWORD'|" config/.env
+sed -i "s|LEAN_DB_DATABASE = ''|LEAN_DB_DATABASE = '$DATABASE_NAME'|" config/.env
 
 # Handle addresses with subdirectories bases URL
 if [ "$INSTALL_URL_PATH" != "/" ]
 then
 	sed -i "s|#RewriteBase /leantime/|RewriteBase $INSTALL_URL_PATH|" public/.htaccess
-	sed -i "s|appUrl = \"\"|appUrl = \"$INSTALL_URL_PATH\"|" config/configuration.php
+	sed -i "s|LEAN_APP_URL = ''|LEAN_APP_URL = '$INSTALL_URL_PATH'|" config/.env
 fi
