3084cd650000e174e2b25a11e56985390ba857eb
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2025-01-22 14:25:22 +0100

    Upgrade Readeck to 0.17.1

--- readeck.sh
+++ readeck.sh
@@ -13,7 +13,7 @@
 set -e
 
 # Download
-wget --no-hsts -O readeck https://codeberg.org/readeck/readeck/releases/download/0.16.0/readeck-0.16.0-linux-amd64
+wget --no-hsts -O readeck https://codeberg.org/readeck/readeck/releases/download/0.17.1/readeck-0.17.1-linux-amd64
 
 chmod +x  readeck
 
