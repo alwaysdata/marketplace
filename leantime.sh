731f5b6c7b68f6791e4b8862423177b4b010f922
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2025-02-27 15:07:22 +0100

    Upgrade Leantime to 3.4.3

--- leantime.sh
+++ leantime.sh
@@ -13,7 +13,7 @@
 set -e
 
 # Download
-wget -O- --no-hsts https://github.com/Leantime/leantime/releases/download/v3.4.2/Leantime-v3.4.2.tar.gz | tar -xz --strip-components=1
+wget -O- --no-hsts https://github.com/Leantime/leantime/releases/download/v3.4.3/Leantime-v3.4.3.tar.gz | tar -xz --strip-components=1
 
 # Configuration
 cp config/sample.env config/.env
