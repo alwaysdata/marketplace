278492b4d6b5d590596959f7ad2a27c85a34962c
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2025-04-01 15:43:22 +0200

    Review Backdrop install script to use bee cli

--- backdrop.sh
+++ backdrop.sh
@@ -4,24 +4,56 @@
 # site:
 #     type: php
 #     path: '{INSTALL_PATH_RELATIVE}'
-#     php_version: '8.1'
+#     php_version: '8.3'
 # database:
 #     type: mysql
 # requirements:
-#     disk: 50
+#     disk: 60
+# form:
+#     language:
+#         type: choices
+#         label:
+#             en: Language
+#             fr: Langue
+#         choices:
+#             de: Deutsch
+#             en: English
+#             es: Español
+#             fr: Français
+#             it: Italiano
+#     site_name:
+#         label:
+#             en: Site name
+#             fr: Nom du site
+#         max_length: 255
+#     email:
+#         type: email
+#         label:
+#             en: Email
+#             fr: Email
+#         max_length: 255
+#     admin_username:
+#         label:
+#             en: Administrator username
+#             fr: Nom d'utilisateur de l'administrateur
+#         regex: ^[ a-zA-Z0-9.+_-]+$
+#         regex_text:
+#             en: "It can include uppercase, lowercase, numbers, spaces, and special characters: .+_-."
+#             fr: "Il peut comporter des majuscules, des minuscules, des chiffres, des espaces et les caractères spéciaux : .+_-."
+#         max_length: 255
+#     admin_password:
+#         type: password
+#         label:
+#             en: Administrator password
+#             fr: Mot de passe de l'administrateur
+#         min_length: 5
+#         max_length: 255
 
 set -e
 
 # https://docs.backdropcms.org/documentation/system-requirements
+# https://github.com/backdrop-contrib/bee/wiki/Usage
 
-# Download
-COMPOSER_CACHE_DIR=/dev/null composer2 create-project backdrop/backdrop default --no-plugins
-
-# Configuration
-sed -i "s|user:pass@localhost/database_name|$DATABASE_USERNAME:$DATABASE_PASSWORD@$DATABASE_HOST/$DATABASE_NAME|" default/settings.php
-
-# Clean the install environment
-rm -rf .config .local .subversion
-shopt -s dotglob
-mv default/* .
-rmdir default
+wget --no-hsts https://github.com/backdrop-contrib/bee/releases/download/1.x-1.1.0/bee.phar
+php bee.phar download-core
+php bee.phar install --db-name=$DATABASE_NAME --db-user=$DATABASE_USERNAME --db-pass=$DATABASE_PASSWORD --db-host=$DATABASE_HOST --username=$FORM_ADMIN_USERNAME --password=$FORM_ADMIN_PASSWORD --email=$FORM_EMAIL --site-mail=$USER@$RESELLER_DOMAIN --langcode=$FORM_LANGUAGE --site-name=$FORM_SITE_NAME --auto
