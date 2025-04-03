932aaf30f8ccbac9457b9415185d61e097b8bcbd
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2025-04-03 14:52:25 +0200

    Upgrafe Leantime to 3.4.8

--- leantime.sh
+++ leantime.sh
@@ -13,7 +13,7 @@
 set -e
 
 # Download
-wget -O- --no-hsts https://github.com/Leantime/leantime/releases/download/v3.4.7/Leantime-v3.4.7.tar.gz | tar -xz --strip-components=1
+wget -O- --no-hsts https://github.com/Leantime/leantime/releases/download/v3.4.8/Leantime-v3.4.8.tar.gz | tar -xz --strip-components=1
 
 # Configuration
 cp config/sample.env config/.env
