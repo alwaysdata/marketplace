420839ada7812b558b7cbbe6078712c51bcc9544
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2025-07-08 11:52:16 +0200

    Upgrade FileBrowser to 2.36.3

--- filebrowser.sh
+++ filebrowser.sh
@@ -18,11 +18,12 @@
 #         label:
 #             en: Password
 #             fr: Mot de passe
+#         min_length: 12
 #         max_length: 255
 
 set -e
 
-wget -O- --no-hsts https://github.com/filebrowser/filebrowser/releases/download/v2.33.8/linux-amd64-filebrowser.tar.gz | tar -xz --strip-components=0
+wget -O- --no-hsts https://github.com/filebrowser/filebrowser/releases/download/v2.36.3/linux-amd64-filebrowser.tar.gz | tar -xz --strip-components=0
 
 ./filebrowser config init -a "0.0.0.0" -p "$PORT" -b "$INSTALL_URL_PATH"
 ./filebrowser users add $FORM_USERNAME $FORM_PASSWORD
