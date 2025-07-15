3bb9d16168b0336c12c47c741a63b218c5d2e7fb
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2025-07-15 17:26:13 +0200

    Meilisearch: adding documentation links

--- meilisearch.sh
+++ meilisearch.sh
@@ -9,14 +9,21 @@
 #     disk: 130
 
 set -e
+
 # Install the latest self-hosted Meilisearch
 curl -L https://install.meilisearch.com | sh
+
 # Fetch the default configuration file
+# All options: https://www.meilisearch.com/docs/learn/self_hosted/configure_meilisearch_at_launch#configuration-file
 curl https://raw.githubusercontent.com/meilisearch/meilisearch/latest/config.toml > config.toml
 sed -i "s|localhost:7700|$IP:$PORT|" config.toml
+
+# Security: https://www.meilisearch.com/docs/learn/security/basic_security
 # Generate and save a random master key
+# This key must not be used as an API key
 master_key=`uuidgen`
 sed -i "s|# master_key = \"YOUR_MASTER_KEY_VALUE\"|master_key = \"$master_key\"|" config.toml
-timeout 5s ./meilisearch --config-file-path="./config.toml" ||true 
-# Fetch and store the default API keys in api_keys.json
+timeout 5s ./meilisearch --config-file-path="./config.toml" ||true
+
+# Fetch and store the default API keys ("Default Search API Key" and "Default Admin API Key") in api_keys.json
 curl -X GET http://$INSTALL_URL/keys -H "Authorization: Bearer $master_key" > api_keys.json
