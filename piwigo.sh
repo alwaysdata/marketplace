43857eb7aef41b1ee7e93e6736784cbd5be98aed
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2025-07-28 14:11:49 +0200

    Upgrade Piwigo to 15.6.0

--- piwigo.sh
+++ piwigo.sh
@@ -33,6 +33,6 @@ set -e
 
 # https://piwigo.org/guides/install/requirements
 
-wget -O- --no-hsts https://github.com/Piwigo/Piwigo/archive/refs/tags/15.5.0.tar.gz| tar -xz --strip-components=1
+wget -O- --no-hsts https://github.com/Piwigo/Piwigo/archive/refs/tags/15.6.0.tar.gz| tar -xz --strip-components=1
 
 curl -X POST -F language="en_US" -F dbhost="$DATABASE_HOST" -F dbuser="$DATABASE_USERNAME" -F dbpasswd="$DATABASE_PASSWORD" -F dbname="$DATABASE_NAME" -F admin_name="$FORM_USERNAME" -F admin_pass1="$FORM_PASSWORD" -F admin_pass2="$FORM_PASSWORD" -F admin_mail="$FORM_EMAIL" -F newsletter_subscribe=none -F send_credentials_by_mail=checked -F install=Start\ installation http://$INSTALL_URL/install.php
