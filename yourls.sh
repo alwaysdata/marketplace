dae4c4570abe6259f8e0954aad02291daa53ab0a
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2025-08-05 15:25:56 +0200

    Upgrade Yourls to 1.10.2

--- yourls.sh
+++ yourls.sh
@@ -33,7 +33,7 @@ set -e
 # https://yourls.org/docs#requirements
 
 # Download
-wget -O- --no-hsts https://github.com/YOURLS/YOURLS/archive/refs/tags/1.10.1.tar.gz| tar -xz --strip-components=1
+wget -O- --no-hsts https://github.com/YOURLS/YOURLS/archive/refs/tags/1.10.2.tar.gz| tar -xz --strip-components=1
 
 # Configuration
 mv user/config-sample.php user/config.php
