bf83cdba49f81cb93d4c3192869fca6113910aec
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2025-08-05 15:23:15 +0200

    Mininote: upgrade to 1.0.4

--- mininote.sh
+++ mininote.sh
@@ -3,17 +3,16 @@
 # Declare site in YAML, as documented on the documentation: https://help.alwaysdata.com/en/marketplace/build-application-script/
 # site:
 #     type: nodejs
-#     nodejs_version: '18'
+#     nodejs_version: '22'
 #     working_directory: '{INSTALL_PATH_RELATIVE}'
-#     command: './node_modules/yarn/bin/yarn start'
+#     command: 'yarn start'
 #     environment: HOME={INSTALL_PATH}
+#     ssl_force: true
 # requirements:
-#     disk: 1000
+#     disk: 800
 
 set -e
 
-wget -O- --no-hsts https://github.com/muety/mininote/archive/refs/tags/1.0.3.tar.gz |tar -xz --strip-components=1
-npm install yarn
-
-~/node_modules/yarn/bin/yarn
-cd webapp && ~/node_modules/yarn/bin/yarn && ~/node_modules/yarn/bin/yarn build
+wget -O- --no-hsts https://github.com/muety/mininote/archive/refs/tags/1.0.4.tar.gz |tar -xz --strip-components=1
+yarn
+cd webapp && yarn && yarn build
