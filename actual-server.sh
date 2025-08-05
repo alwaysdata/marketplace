acbc8d0aec8169a0a5b78f1de5202d66f1f9495d
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2025-08-05 15:17:17 +0200

    Actual: Change repository and upgrade

--- actual-server.sh
+++ actual-server.sh
@@ -3,33 +3,34 @@
 # Declare site in YAML, as documented on the documentation: https://help.alwaysdata.com/en/marketplace/build-application-script/
 # site:
 #     type: nodejs
-#     nodejs_version: '20'
+#     nodejs_version: '22'
 #     working_directory: '{INSTALL_PATH_RELATIVE}'
-#     command: 'node ./app.js'
+#     command: 'yarn start:server'
+#     environment: |
+#         HOME={INSTALL_PATH}
 #     path_trim: true
 #     ssl_force: true
 # requirements:
-#     disk: 440
+#     disk: 2300
 
 set -e
 
-VERSION=25.2.0
-
-# https://github.com/actualbudget/actual-server#running
+# https://actualbudget.org/docs/actual-server-repo-move/
+# https://actualbudget.org/docs/install/build-from-source
 
 # Download and install
-git clone -b v$VERSION --depth 1 https://github.com/actualbudget/actual-server.git $INSTALL_PATH
+git clone -b v25.8.0 --depth 1 https://github.com/actualbudget/actual .
 
-npm install yarn
-$HOME/node_modules/yarn/bin/yarn install
+yarn install
 
-#Configuration
-cat << EOF > config.json
+# Configuration
+cat << EOF > packages/sync-server/config.json
 {
-    "mode": "production",
     "port": $PORT,
     "hostname": "::",
-    "serverFiles": "$INSTALL_PATH/server-files",
-    "userFiles": "$INSTALL_PATH/user-files"
+    "serverFiles": "$INSTALL_PATH/packages/sync-server/server-files",
+    "userFiles": "$INSTALL_PATH/packages/sync-server/user-files"
 }
 EOF
+
+yarn build:server
