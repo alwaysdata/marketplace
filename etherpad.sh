e31828ede48429c5b6ea41ccf384932c7d348a4b
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2025-08-05 15:17:50 +0200

    Etherpad: update to 2.4.2

--- etherpad.sh
+++ etherpad.sh
@@ -3,22 +3,24 @@
 # Declare site in YAML, as documented on the documentation: https://help.alwaysdata.com/en/marketplace/build-application-script/
 # site:
 #     type: nodejs
-#     nodejs_version: '20'
+#     nodejs_version: '22'
 #     working_directory: '{INSTALL_PATH_RELATIVE}'
-#     command: 'node src/node/server.js'
+#     command: 'pnpm run prod'
 #     environment: |
 #         NODE_ENV=production
 #         HOME={INSTALL_PATH}
 # database:
 #     type: mysql
 # requirements:
-#     disk: 270
+#     disk: 1000
 
 set -e
 
 # https://github.com/ether/etherpad-lite#installation
 
-wget -O- --no-hsts https://github.com/ether/etherpad-lite/archive/1.9.7.tar.gz | tar -xz --strip-components=1
+npm install pnpm
+
+wget -O- --no-hsts https://github.com/ether/etherpad-lite/archive/2.4.2.tar.gz | tar -xz --strip-components=1
 
 cp settings.json.template settings.json
 
@@ -38,5 +40,5 @@ cat << EOF > credentials.json
   },
 }
 EOF
-
-./src/bin/installDeps.sh
+pnpm i
+pnpm run build:etherpad
