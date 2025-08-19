18f3c13e036d8789cb3cc8c776e6c8c3f1d460db
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2025-08-19 11:13:21 +0200

    Upgrade Shaarli to 0.15.0

--- shaarli.sh
+++ shaarli.sh
@@ -37,6 +37,6 @@
 
 set -e
 
-wget -O- --no-hsts https://github.com/shaarli/Shaarli/releases/download/v0.14.0/shaarli-v0.14.0-full.tar.gz | tar -xz --strip-components=1
+wget -O- --no-hsts https://github.com/shaarli/Shaarli/releases/download/v0.15.0/shaarli-v0.15.0-full.tar.gz | tar -xz --strip-components=1
 
 curl -X POST -F setlogin="$FORM_USERNAME" -F setpassword="$FORM_PASSWORD" -F title="$FORM_TITLE" -F language="$FORM_LANGUAGE" -F Save=Install http://$INSTALL_URL/install
