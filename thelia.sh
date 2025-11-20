488b2931358f5803123c26ab7b19919c8da269ba
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2025-11-20 10:35:38 +0100

    Fix Thelia script with upgrade to 2.6.0

--- thelia.sh
+++ thelia.sh
@@ -4,11 +4,15 @@
 # site:
 #     type: php
 #     path: '{INSTALL_PATH_RELATIVE}/web'
-#     php_version: '8.1'
+#     php_version: '8.3'
+#     vhost_additional_directives: |
+#         <IfModule mod_brotli.c>
+#         BrotliCompressionQuality 5
+#         </IfModule>
 # database:
 #     type: mysql
 # requirements:
-#     disk: 200
+#     disk: 250
 # form:
 #     email:
 #         type: email
@@ -43,15 +47,21 @@ set -e
 # https://github.com/thelia/thelia-project?tab=readme-ov-file#compatibility
 
 # Download
-echo 'n' | COMPOSER_CACHE_DIR=/dev/null composer2 create-project thelia/thelia-project default
+COMPOSER_CACHE_DIR=/dev/null composer2 create-project thelia/thelia-project --no-install default
 
 cd default
+
+COMPOSER_CACHE_DIR=/dev/null composer2 config --no-plugins allow-plugins.symfony/runtime true
+echo "n"|COMPOSER_CACHE_DIR=/dev/null composer2 install
 
 # Install
 php Thelia thelia:install --db_host "$DATABASE_HOST" --db_username "$DATABASE_USERNAME" --db_password "$DATABASE_PASSWORD" --db_name "$DATABASE_NAME"
 
 # Create admin user
 php Thelia admin:create -q --login_name "$FORM_ADMIN_USERNAME" --first_name "$FORM_ADMIN_FIRSTNAME" --last_name "$FORM_ADMIN_LASTNAME" --email "$FORM_EMAIL" --password "$FORM_ADMIN_PASSWORD"
+
+# BrotliCompressionQuality n'est pas autorisé dans un .htaccess: https://httpd.apache.org/docs/current/en/mod/mod_brotli.html#brotlicompressionquality
+sed -i '/BrotliCompressionQuality 5/d' web/.htaccess
 
 # Clean install environment
 cd ..
