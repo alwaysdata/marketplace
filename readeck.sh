45f4a9fc0512f05ae03dcd40f83ad726ab9e8c1a
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2025-12-03 13:40:26 +0100

    Upgrade Readeck to 0.21.3

--- readeck.sh
+++ readeck.sh
@@ -31,7 +31,7 @@
 set -e
 
 # Download
-wget --no-hsts -O readeck https://codeberg.org/readeck/readeck/releases/download/0.21.2/readeck-0.21.2-linux-amd64
+wget --no-hsts -O readeck https://codeberg.org/readeck/readeck/releases/download/0.21.3/readeck-0.21.3-linux-amd64
 
 chmod +x  readeck
 
