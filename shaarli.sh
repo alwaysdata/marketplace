3bfd6ef3447098221a7ccfc0542e967a1efaf5b3
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2026-01-26 16:08:40 +0100

    Upgrade Shaarli to 0.16.1

--- shaarli.sh
+++ shaarli.sh
@@ -4,7 +4,7 @@
 # site:
 #     type: php
 #     path: '{INSTALL_PATH_RELATIVE}'
-#     php_version: '8.2'
+#     php_version: '8.4'
 # requirements:
 #     disk: 20
 # form:
@@ -39,6 +39,6 @@ set -e
 
 # https://shaarli.readthedocs.io/en/latest/Server-configuration.html
 
-wget -O- --no-hsts https://github.com/shaarli/Shaarli/releases/download/v0.15.0/shaarli-v0.15.0-full.tar.gz | tar -xz --strip-components=1
+wget -O- --no-hsts https://github.com/shaarli/Shaarli/releases/download/v0.16.1/shaarli-v0.16.1-full.tar.gz | tar -xz --strip-components=1
 
 curl -X POST -F setlogin="$FORM_USERNAME" -F setpassword="$FORM_PASSWORD" -F title="$FORM_TITLE" -F language="$FORM_LANGUAGE" -F Save=Install http://$INSTALL_URL/install
