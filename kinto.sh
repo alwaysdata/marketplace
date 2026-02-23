6d7ff32ae113b4f6ef011d8c6316adfbe1023b8a
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2026-02-23 14:39:48 +0100

    Upgrade Kinto to 25.0.0

--- kinto.sh
+++ kinto.sh
@@ -2,29 +2,31 @@
 
 # Declare site in YAML, as documented on the documentation: https://help.alwaysdata.com/en/marketplace/build-application-script/
 # site:
-#     type: wsgi
-#     path: '{INSTALL_PATH_RELATIVE}/app.wsgi'
-#     python_version: '3.10'
-#     virtualenv_directory: '{INSTALL_PATH_RELATIVE}/env'
+#     type: user_program
 #     working_directory: '{INSTALL_PATH_RELATIVE}'
+#     command: './env/bin/kinto start'
+#     environment: |
+#         PYTHON_VERSION=3.11
 #     ssl_force: true
 # database:
 #     type: postgresql
 # requirements:
-#     disk: 85
+#     disk: 120
 
 set -e
+
+export PYTHON_VERSION=3.11
 
 # Create virtualenv and install Kinto in it
 python -m venv env
 source env/bin/activate
 python -m pip install kinto[postgresql]
 
-# WSGI
-wget --no-hsts https://raw.githubusercontent.com/Kinto/kinto/master/app.wsgi
-
 # Configuration
 # https://docs.kinto-storage.org/en/latest/configuration/settings.html
 kinto init --ini config/kinto.ini --backend postgresql --cache-backend postgresql
 sed -i "s|postgresql://postgres:postgres@localhost/postgres|postgresql://$DATABASE_USERNAME:$DATABASE_PASSWORD@$DATABASE_HOST/$DATABASE_NAME|" config/kinto.ini
+sed -i "s|port = %(http_port)s|port = $PORT|" config/kinto.ini
+sed -i "s|host = 127.0.0.1|host = ::|" config/kinto.ini
+
 kinto migrate --ini config/kinto.ini
