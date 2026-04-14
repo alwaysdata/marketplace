38cafd166af5086e184b45d1d87288e2bef7dab7
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2026-04-14 10:50:42 +0200

    Upgrade Dolibarr to 23.0.2

--- dolibarr.sh
+++ dolibarr.sh
@@ -15,20 +15,27 @@
 #             en: Username
 #             fr: Nom d'utilisateur
 #         max_length: 255
+#     password:
+#         type: password
+#         label:
+#             en: Password
+#             fr: Mot de passe
+#         max_length: 255
 
 set -e
 
 # https://wiki.dolibarr.org/index.php?title=Prerequisites
 
-wget -O- https://downloads.sourceforge.net/project/dolibarr/Dolibarr%20ERP-CRM/23.0.0/dolibarr-23.0.0.zip | bsdtar --strip-components=1 -xf -
+wget -O- https://downloads.sourceforge.net/project/dolibarr/Dolibarr%20ERP-CRM/23.0.2/dolibarr-23.0.2.zip | bsdtar --strip-components=1 -xf -
 
 mkdir documents
 cp htdocs/install/install.forced.sample.php htdocs/install/install.forced.php 
-sed -i "s|force_install_database = 'dolibarr';|force_install_database = '$DATABASE_NAME';|" htdocs/install/install.forced.php
-sed -i "s|force_install_dbserver = 'localhost';|force_install_dbserver = '$DATABASE_HOST';|" htdocs/install/install.forced.php
+sed -i "s|force_install_database = getenv('DOLI_DATABASE', true) ?: 'dolibarr';|force_install_database = getenv('DOLI_DATABASE', true) ?: '$DATABASE_NAME';|" htdocs/install/install.forced.php
+sed -i "s|force_install_dbserver = getenv('DOLI_DB_SERVER', true) ?: 'localhost';|force_install_dbserver = getenv('DOLI_DB_SERVER', true) ?: '$DATABASE_HOST';|" htdocs/install/install.forced.php
 sed -i "s|force_install_databaselogin = 'root';|force_install_databaselogin = '$DATABASE_USERNAME';|" htdocs/install/install.forced.php
-sed -i "s|force_install_databasepass = '';|force_install_databasepass = '$DATABASE_PASSWORD';|" htdocs/install/install.forced.php
+sed -i "s|force_install_databasepass = getenv('DOLI_DB_PASSWORD', true) ?: '';|force_install_databasepass = getenv('DOLI_DB_PASSWORD', true) ?: '$DATABASE_PASSWORD';|" htdocs/install/install.forced.php
 sed -i "s|force_install_databaserootlogin = 'root';|force_install_databaserootlogin = '$DATABASE_USERNAME';|" htdocs/install/install.forced.php
-sed -i "s|force_install_databaserootpass = '';|force_install_databaserootpass = '$DATABASE_PASSWORD';|" htdocs/install/install.forced.php
+sed -i "s|force_install_databaserootpass = getenv('DOLI_ROOT_PASSWORD', true) ?: '';|force_install_databaserootpass = getenv('DOLI_ROOT_PASSWORD', true) ?: '$DATABASE_PASSWORD';|" htdocs/install/install.forced.php
 sed -i "s|force_install_dolibarrlogin = 'admin';|force_install_dolibarrlogin = '$FORM_USERNAME';|" htdocs/install/install.forced.php
+sed -i "s|force_install_dolibarrpassword = '';|force_install_dolibarrpassword = '$FORM_PASSWORD';|" htdocs/install/install.forced.php
 sed -i "s|force_install_createdatabase = true;|force_install_createdatabase = false;|" htdocs/install/install.forced.php
