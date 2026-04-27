9370539e116f83cfe97793409d9928d8453ab269
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2026-04-27 12:00:18 +0200

    Upgrade Gitea to 1.26.1

--- gitea.sh
+++ gitea.sh
@@ -17,7 +17,7 @@ set -e
 # https://docs.gitea.io/en-us/install-from-binary/
 
 # Download
-wget  --no-hsts -O gitea https://github.com/go-gitea/gitea/releases/download/v1.26.0/gitea-1.26.0-linux-amd64
+wget  --no-hsts -O gitea https://github.com/go-gitea/gitea/releases/download/v1.26.1/gitea-1.26.1-linux-amd64
 chmod +x gitea
 
 # Configuration
