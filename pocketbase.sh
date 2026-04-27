c22033bd19368b43aab0ada9cbc581352a42e36b
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2026-04-27 12:01:09 +0200

    Upgrade Pocketbase to 0.437.4

--- pocketbase.sh
+++ pocketbase.sh
@@ -26,7 +26,7 @@ set -e
 
 # https://pocketbase.io/docs/
 
-wget -O- --no-hsts https://github.com/pocketbase/pocketbase/releases/download/v0.37.2/pocketbase_0.37.2_linux_amd64.zip | bsdtar --strip-components=0 -xf -
+wget -O- --no-hsts https://github.com/pocketbase/pocketbase/releases/download/v0.37.4/pocketbase_0.37.4_linux_amd64.zip | bsdtar --strip-components=0 -xf -
 chmod +x pocketbase
 
 ./pocketbase superuser upsert $FORM_EMAIL $FORM_PASSWORD
