bfdac9a9df8dc0a967e9def3d91c8f8d58d923ee
Héloïse Degorce-Dumas <hdegorce@alwaysdata.com>
2026-07-21 14:21:34 +0200

    Upgrade Forgejo to 16.0.0

--- forgejo.sh
+++ forgejo.sh
@@ -15,7 +15,7 @@
 set -e
 
 # Download
-wget --no-hsts -O forgejo https://codeberg.org/forgejo/forgejo/releases/download/v15.0.5/forgejo-15.0.5-linux-amd64
+wget --no-hsts -O forgejo https://codeberg.org/forgejo/forgejo/releases/download/v16.0.0/forgejo-16.0.0-linux-amd64
 
 chmod +x  forgejo
 
